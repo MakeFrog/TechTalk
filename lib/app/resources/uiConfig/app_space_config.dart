323dd65971b79f82513104cc876c31756eb063b7
XimYa <hj_lee_com@naver.com>
2023-04-12 23:47:59 +0900

    feature : app ui resources 추가

---
+++ lib/app/resources/uiConfig/app_space_config.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppSpace {
+  AppSpace._();
+
+  static const size2 = SizedBox(width: 2, height: 2);
+  static const size4 = SizedBox(width: 4, height: 4);
+  static const size6 = SizedBox(width: 6, height: 6);
+  static const size7 = SizedBox(width: 7, height: 7);
+  static const size8 = SizedBox(width: 8, height: 8);
+  static const size10 = SizedBox(width: 10, height: 10);
+  static const size12 = SizedBox(width: 12, height: 12);
+  static const size14 = SizedBox(width: 14, height: 14);
+  static const size16 = SizedBox(width: 16, height: 16);
+  static const size18 = SizedBox(width: 18, height: 18);
+  static const size19 = SizedBox(width: 19, height: 19);
+  static const size20 = SizedBox(width: 20, height: 20);
+  static const size21 = SizedBox(width: 21, height: 21);
+  static const size22 = SizedBox(width: 22, height: 22);
+  static const size24 = SizedBox(width: 24, height: 24);
+  static const size26 = SizedBox(width: 26, height: 26);
+  static const size28 = SizedBox(width: 28, height: 28);
+  static const size30 = SizedBox(width: 30, height: 30);
+  static const size32 = SizedBox(width: 32, height: 32);
+  static const size34 = SizedBox(width: 34, height: 34);
+  static const size36 = SizedBox(width: 36, height: 36);
+  static const size39 = SizedBox(width: 39, height: 39);
+  static const size40 = SizedBox(width: 40, height: 40);
+  static const size48 = SizedBox(width: 48, height: 48);
+  static const size52 = SizedBox(width: 52, height: 52);
+  static const size56 = SizedBox(width: 56, height: 56);
+  static const size64 = SizedBox(width: 64, height: 64);
+  static const size72 = SizedBox(width: 72, height: 72);
+}
---
+++ lib/app/resources/uiConfig/color_config.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  AppColor._();
+
+
+  static const black = Color(0xFF0F0F0F);
+  static const darkGrey = Color(0xFF212121); // Used in Bottom Navigation
+  static const lightGrey = Color(0xFFBBBAC1); // Used in Home Sub title;
+  static const mixedWhite = Color(0xFFE6E6E6);
+  static const strongGrey = Color(0xFF1F1F20);
+  static const yellow = Color(0xFFFFE24B);
+  static const red = Color(0xFFFF484E);
+  static const green = Color(0xFF34C759);
+}
