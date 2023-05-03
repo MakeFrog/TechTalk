b008e883752274fcec80a9ccba87ee73c12ce796
XimYa <hj_lee_com@naver.com>
2023-05-04 08:50:13 +0900

    feature : single import 소스파일 추가
    - 자주 사용되는 모듈 리스트를 한 소스 파일에서 관리

---
+++ lib/utilities/index.dart
@@ -0,0 +1,13 @@
+/** Created By Ximya - 2023.05.04
+ * 자주 사용되는 모듈을 하나의 소스파일에서 관리하여 import를 편리하도록 함.
+ * */
+
+export 'package:flutter/material.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:moon_dap/app/resources/uiConfig/app_insets.dart';
+export 'package:moon_dap/app/resources/uiConfig/app_space_config.dart';
+export 'package:moon_dap/app/resources/uiConfig/color_config.dart';
+export 'package:moon_dap/app/resources/uiConfig/size_config.dart';
+export 'package:moon_dap/presentation/base/base_view.dart';
+export 'package:moon_dap/presentation/common/bubble/new_chat_bubble.dart';
+export 'package:provider/provider.dart';
