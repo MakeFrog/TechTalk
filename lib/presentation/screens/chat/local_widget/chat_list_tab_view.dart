8992b3e6a650b5b8f287db63761dc55d98b2bae6
XimYa <hj_lee_com@naver.com>
2023-05-04 08:49:37 +0900

    refactor : chatscreen chatlistTabView 리팩토링
    - 모듈 분리

---
+++ lib/presentation/screens/chat/local_widget/chat_list_tab_view.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:moon_dap/app/resources/uiConfig/app_insets.dart';
+import 'package:moon_dap/app/resources/uiConfig/app_space_config.dart';
+import 'package:moon_dap/app/resources/uiConfig/color_config.dart';
+import 'package:moon_dap/app/resources/uiConfig/size_config.dart';
+import 'package:moon_dap/domain/model/chat/chat.dart';
+import 'package:moon_dap/presentation/base/base_view.dart';
+import 'package:moon_dap/presentation/common/bubble/new_chat_bubble.dart';
+import 'package:moon_dap/presentation/screens/chat/chat_view_model.dart';
+import 'package:provider/provider.dart';
+
+class ChatListTabView extends BaseView<ChatViewModel> {
+  const ChatListTabView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Selector<ChatViewModel, List<Chat>>(
+            selector: (BuildContext context, ChatViewModel vm) {
+              return vm.chatList;
+            },
+            builder: (context, chatList, child) {
+              return GestureDetector(
+                onTap: vm(context).onFocusKeyboard,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: ListView.separated(
+                    reverse: true,
+                    shrinkWrap: true,
+                    controller: vm(context).firstTabScrollController,
+                    padding: AppInset.top12 +
+                        AppInset.horizontal12 +
+                        AppInset.bottom16,
+                    itemCount: chatList.length,
+                    itemBuilder: (_, index) {
+                      return NewChatBubble(
+                        messageType: chatList[index].type,
+                        message: chatList[index].message,
+                      );
+                    },
+                    separatorBuilder: (_, __) => AppSpace.size14,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        const _StackedInputField(),
+      ],
+    );
+  }
+}
+
+/// 하단 고정 텍스트 입력창
+class _StackedInputField extends BaseView<ChatViewModel> {
+  const _StackedInputField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(minHeight: 48),
+      width: SizeConfig.to.screenWidth,
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Color(0xFFE5E5EA),
+          ),
+        ),
+      ),
+      child: Stack(
+        children: [
+          TextField(
+            focusNode: vm(context).focusNode,
+            onChanged: vm(context).onFieldChanged,
+            controller: vm(context).textEditingController,
+            maxLines: null,
+            textAlignVertical: TextAlignVertical.top,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: const EdgeInsets.only(
+                right: 42,
+                left: 16,
+                top: 18,
+              ),
+              // ⚠️ suffix icon의 inkwell 효과를 고려하고 싶다면, TextField의 Color를 trasnparent로 설정해야됨
+              hintText: '답변을 입력하세요',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+          ),
+          // suffix 전송 버튼
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/send.svg",
+                colorFilter: ColorFilter.mode(
+                  vmS(
+                    context,
+                    (value) => value.isTextField
+                        ? AppColor.blue
+                        : const Color(0xFFBDBDC2),
+                  ),
+                  BlendMode.srcIn,
+                ),
+              ),
+              onPressed: vm(context).onFieldSubmitted,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
