36c7bd5a9db6a1302656f9c306da8718b66ef1fc
XimYa <hj_lee_com@naver.com>
2023-05-06 17:17:26 +0900

    feature : 채팅 UI 불필요 코드 제거

--- lib/presentation/common/bubble/stream_base_chat_bubble.dart
+++ lib/presentation/common/bubble/stream_base_chat_bubble.dart
@@ -29,113 +29,31 @@ class StreamChatBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: alignmentOnType,
       children: [
-        if (ChatMessageType.answerQuestion != messageType)
-          const CircleAvatar(
-            backgroundImage: AssetImage("assets/images/avatar_1.png"),
-          ),
+        const CircleAvatar(
+          backgroundImage: AssetImage("assets/images/avatar_1.png"),
+        ),
         Container(
           margin: margin ?? EdgeInsets.zero,
           child: PhysicalShape(
-            clipper: clipperOnType,
+            clipper: ChatBubbleClipper1(type: BubbleType.receiverBubble),
             elevation: 2,
-            color: bgColorOnType,
+            color: const Color(0xffE7E7ED),
             shadowColor: Colors.grey.shade200,
             child: Container(
               constraints: BoxConstraints(
                 maxWidth: SizeConfig.to.screenWidth * 0.8,
               ),
-              padding: paddingOnType,
+              padding: const EdgeInsets.only(
+                  top: 10, bottom: 10, left: 20, right: 10),
               child: Text(
                 message,
-                style: TextStyle(color: textColorOnType),
+                style: const TextStyle(color: AppColor.black),
               ),
             ),
           ),
         ),
       ],
     );
-  }
-
-  Color get textColorOnType {
-    switch (messageType) {
-      case ChatMessageType.alertMessage:
-        return AppColor.black;
-
-      case ChatMessageType.replyToUserAnswer:
-        return AppColor.black;
-
-      case ChatMessageType.askQuestion:
-        return AppColor.black;
-
-      case ChatMessageType.replyToUserAnswerDone:
-        return AppColor.black;
-
-      case ChatMessageType.answerQuestion:
-        return AppColor.white;
-    }
-  }
-
-  Color get bgColorOnType {
-    switch (messageType) {
-      case ChatMessageType.alertMessage:
-        return const Color(0xffE7E7ED);
-      case ChatMessageType.replyToUserAnswer:
-        return const Color(0xffE7E7ED);
-      case ChatMessageType.askQuestion:
-        return const Color(0xffE7E7ED);
-      case ChatMessageType.replyToUserAnswerDone:
-        return const Color(0xffE7E7ED);
-
-      case ChatMessageType.answerQuestion:
-        return AppColor.blue;
-    }
-  }
-
-  CustomClipper<Path> get clipperOnType {
-    switch (messageType) {
-      case ChatMessageType.alertMessage:
-        return ChatBubbleClipper1(type: BubbleType.receiverBubble);
-      case ChatMessageType.replyToUserAnswer:
-        return ChatBubbleClipper1(type: BubbleType.receiverBubble);
-      case ChatMessageType.askQuestion:
-        return ChatBubbleClipper1(type: BubbleType.receiverBubble);
-      case ChatMessageType.replyToUserAnswerDone:
-        return ChatBubbleClipper1(type: BubbleType.receiverBubble);
-
-      case ChatMessageType.answerQuestion:
-        return ChatBubbleClipper1(type: BubbleType.sendBubble);
-    }
-  }
-
-  MainAxisAlignment get alignmentOnType {
-    switch (messageType) {
-      case ChatMessageType.alertMessage:
-        return MainAxisAlignment.start;
-      case ChatMessageType.replyToUserAnswer:
-        return MainAxisAlignment.start;
-      case ChatMessageType.askQuestion:
-        return MainAxisAlignment.start;
-      case ChatMessageType.replyToUserAnswerDone:
-        return MainAxisAlignment.start;
-      case ChatMessageType.answerQuestion:
-        return MainAxisAlignment.end;
-    }
-  }
-
-  EdgeInsets get paddingOnType {
-    switch (messageType) {
-      case ChatMessageType.alertMessage:
-        return const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 10);
-      case ChatMessageType.askQuestion:
-        return const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 10);
-      case ChatMessageType.replyToUserAnswerDone:
-        return const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 10);
-      case ChatMessageType.answerQuestion:
-        return const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 20);
-      case ChatMessageType.replyToUserAnswer:
-        return const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 10);
-    }
   }
 }
