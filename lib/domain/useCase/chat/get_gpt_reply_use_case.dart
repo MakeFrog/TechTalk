ba30eb34215ff9a45bcdf12026a4b00a74048094
XimYa <hj_lee_com@naver.com>
2023-05-06 17:16:39 +0900

    feature : chat gpt 응답 로직 고도화
    - 응답 진행 상태 관리
    - viewmodel로부터 callback을 받아 정답 여부를 리턴할 수 있도록 변경
    
    **prompt는 지속적으로 업데이트 해야함**

--- lib/domain/useCase/chat/get_gpt_reply_use_case.dart
+++ lib/domain/useCase/chat/get_gpt_reply_use_case.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:chatgpt_completions/chatgpt_completions.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rxdart/rxdart.dart';
@@ -8,8 +9,15 @@ import 'package:rxdart/rxdart.dart';
  *  View레이어에서 독립적인 Stream 이벤트를 fetch 할 수 있도록 함.
  * */
 
+enum ReplyState {
+  init,
+  checking,
+  answerChecked,
+}
+
 class GetGptReplyUseCase {
   final ChatGPTCompletions completions = ChatGPTCompletions.instance;
+  ReplyState state = ReplyState.init;
 
   /// ChatGpt 응답을 요청하는 메소드
   /// [BehaviorSubject]을 리턴하여 독립적인 Stream 객체를 반환
@@ -17,11 +25,14 @@ class GetGptReplyUseCase {
     required String category,
     required String question,
     required String userAnswer,
+    required VoidCallback onStreamDone,
+    required void Function(bool) checkAnswer,
   }) {
     final BehaviorSubject<String> messageSubject = BehaviorSubject<String>();
+    state = ReplyState.checking;
 
     final prompt =
-        '$category 프로그래밍 분야에서 "$question"라는 프로그래밍 질문에,\n"$userAnswer" 라고 답한다면 옳은 답변일까?\n정답이라면 "[correct]" 오답이라면 "[incorrect]"라는 태그 단어를 문장 맨 앞에 붙여주고 이유도 간략하게 설명해줘.';
+        '$category 프로그래밍 분야에서 "$question"라는 프로그래밍 질문에,\n"$userAnswer" 라고 답한다면 옳은 답변일까?\n정답이라면 "[correct]" 오답이라면 "[wrong]"라는 태그 단어를 문장 맨 앞에 붙여주고 이유도 간략하게 설명해줘.';
     completions.textCompletions(
       TextCompletionsParams(
         prompt: prompt,
@@ -32,14 +43,32 @@ class GetGptReplyUseCase {
         stream: true,
         maxTokens: 500,
       ),
-      onStreamValue: (characters) {
-        final filteredRes = formatString(characters);
+      onStreamValue: (response) {
+        setCorrectness(response, checkAnswer);
+        final filteredRes = formatString(response);
         messageSubject.add(filteredRes);
-        print(characters);
+        print(response);
       },
-    ).then((_) => messageSubject.close());
+    ).then((_) {
+      messageSubject.close();
+      state = ReplyState.init;
+      onStreamDone();
+    });
 
     return messageSubject;
+  }
+
+  /// 정답 여부 확인 상태 변경
+  void setCorrectness(String response, Function(bool) checkAnswer) {
+    if (state == ReplyState.checking && response.length >= 10) {
+      if (response.contains("[wrong]")) {
+        checkAnswer(false);
+      } else {
+        checkAnswer(true);
+      }
+
+      state = ReplyState.answerChecked;
+    }
   }
 
   /// 문자열 포맷
@@ -49,12 +78,12 @@ class GetGptReplyUseCase {
   String formatString(String response) {
     String originText = response.replaceAll("\n", '');
 
-    if (originText.contains("[incorrect]")) {
-      String cleanedText = originText.replaceFirst("[incorrect]", "").trim();
-      return "오답입니다\n$cleanedText";
+    if (originText.contains("[wrong]")) {
+      String cleanedText = originText.replaceFirst("[wrong]", "").trim();
+      return "[오답]\n$cleanedText";
     } else {
       String cleanedText = originText.replaceFirst("[correct]", "").trim();
-      return "정답니다\n$cleanedText";
+      return "[정답]\n$cleanedText";
     }
   }
 
