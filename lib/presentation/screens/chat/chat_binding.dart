199b0924ebe1d58e884b9d65e1b8d9dcfee09fab
XimYa <hj_lee_com@naver.com>
2023-05-06 17:17:59 +0900

    chore : 불필요 코드 제거

--- lib/presentation/screens/chat/chat_binding.dart
+++ lib/presentation/screens/chat/chat_binding.dart
@@ -1,10 +1,10 @@
 import 'package:moon_dap/app/routes/go_route_with_binding.dart';
+import 'package:moon_dap/domain/useCase/chat/get_gpt_reply_use_case.dart';
+import 'package:moon_dap/main.dart';
 
 class ChatBinding extends Bindings {
   @override
-  void dependencies() {
-    // TODO: implement dependencies
-  }
+  void dependencies() {}
 
   @override
   void unRegister() {
