c1328eee911eb8d0cb55e11cbe05bd312a24cd62
XimYa <hj_lee_com@naver.com>
2023-05-06 17:16:55 +0900

    feature : intl 리소스 활성화 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:moon_dap/app/resources/uiConfig/size_config.dart';
 import 'package:moon_dap/app/routes/app_pages.dart';
-import 'package:moon_dap/domain/repository/sample_reposiotry.dart';
-import 'package:moon_dap/domain/useCase/sample_use_case.dart';
-import 'package:moon_dap/presentation/screens/home/home_screen.dart';
+import 'package:moon_dap/domain/useCase/chat/get_gpt_reply_use_case.dart';
 import 'package:moon_dap/presentation/screens/responsive_layout_builder.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  setUp();
+  await initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
 final getIt = GetIt.instance;
+
+void setUp() {
+  getIt.registerLazySingleton(() => GetGptReplyUseCase());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: '문답',
