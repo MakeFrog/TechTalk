d68b6227b5cd1f4f4d54a236a167ef6fae8e25d6
suneog <tnsdjr7@gmail.com>
2023-10-20 14:47:46 +0900

    feat(home): 유저 응원 메세지 카드 구현

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:techtalk/app/router/router.dart';
-import 'package:techtalk/presentation/providers/app_user_auth_provider.dart';
+import 'package:techtalk/core/core.dart';
+import 'package:techtalk/core/theme/extension/app_color.dart';
+import 'package:techtalk/presentation/pages/home/widgets/cheer_up_message_card.dart';
 import 'package:techtalk/presentation/providers/app_user_data_provider.dart';
 
 class HomePage extends ConsumerWidget {
@@ -10,6 +12,7 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // 회원가입을 완료하지 않고 홈으로 접속 시 회원가입 페이지로 이동하기 위한 listener
     ref.listen(appUserDataProvider, (_, next) {
       if (next.valueOrNull != null) {
         if (!next.requireValue!.isCompleteSignUp) {
@@ -19,25 +22,19 @@ class HomePage extends ConsumerWidget {
     });
 
     return Scaffold(
+      backgroundColor: AppColor.of.background1,
       appBar: AppBar(
-        actions: [
-          Consumer(
-            builder: (context, ref, child) {
-              return IconButton(
-                onPressed: () async {
-                  await ref.read(appUserAuthProvider.notifier).signOut();
-                },
-                icon: FaIcon(
-                  FontAwesomeIcons.arrowRightFromBracket,
-                ),
-              );
-            },
-          ),
-        ],
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          Assets.logoTechTalkLogo,
+          height: 26,
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8),
         children: [
+          CheerUpMessageCard(),
           Center(
             child: Text('test'),
           ),
