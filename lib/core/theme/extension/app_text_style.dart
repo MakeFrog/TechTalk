286ad003eb7573927e256fa8d4fc4146b18490f1
suneog <tnsdjr7@gmail.com>
2023-10-21 21:09:59 +0900

    refactor: AppTextStyle height 값 수정

--- lib/core/theme/extension/app_text_style.dart
+++ lib/core/theme/extension/app_text_style.dart
@@ -25,16 +25,16 @@ abstract class AppTextStyle {
         fontWeight: FontWeight.w800,
       );
 
-  static final TextStyle highlight = pretendardBoldStyle(32.sp, 36);
-  static final TextStyle headline1 = pretendardBoldStyle(24.sp, 33);
-  static final TextStyle headline2 = pretendardBoldStyle(20.sp, 27);
-  static final TextStyle headline3 = pretendardSemiBoldStyle(18.sp, 24);
-  static final TextStyle title1 = pretendardBoldStyle(16.sp, 22);
-  static final TextStyle title2 = pretendardSemiBoldStyle(16.sp, 22);
-  static final TextStyle title3 = pretendardBoldStyle(14.sp, 20);
-  static final TextStyle body1 = pretendardSemiBoldStyle(14.sp, 20);
-  static final TextStyle body2 = pretendardMediumStyle(14.sp, 20);
-  static final TextStyle body3 = pretendardMediumStyle(13.sp, 18);
-  static final TextStyle alert1 = pretendardSemiBoldStyle(12.sp, 17);
-  static final TextStyle alert2 = pretendardStyle(12.sp, 17);
+  static final TextStyle highlight = pretendardBoldStyle(32.sp, 36.sp);
+  static final TextStyle headline1 = pretendardBoldStyle(24.sp, 33.sp);
+  static final TextStyle headline2 = pretendardBoldStyle(20.sp, 27.sp);
+  static final TextStyle headline3 = pretendardSemiBoldStyle(18.sp, 24.sp);
+  static final TextStyle title1 = pretendardBoldStyle(16.sp, 22.sp);
+  static final TextStyle title2 = pretendardSemiBoldStyle(16.sp, 22.sp);
+  static final TextStyle title3 = pretendardBoldStyle(14.sp, 20.sp);
+  static final TextStyle body1 = pretendardSemiBoldStyle(14.sp, 20.sp);
+  static final TextStyle body2 = pretendardMediumStyle(14.sp, 20.sp);
+  static final TextStyle body3 = pretendardMediumStyle(13.sp, 18.sp);
+  static final TextStyle alert1 = pretendardSemiBoldStyle(12.sp, 17.sp);
+  static final TextStyle alert2 = pretendardStyle(12.sp, 17.sp);
 }
