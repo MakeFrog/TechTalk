510e01f9e5962f1879b6cdab0b3c89c21d97cf7c
XimYa <hj_lee_com@naver.com>
2023-10-24 00:47:40 +0900

    feature : 채팅 리스트 데이터를 관리하는 chatlist provider 추가
    - 채팅 페이지에서 채팅 리스트 데이터를 관리하는 핵심 provider.

---
+++ lib/presentation/pages/chat/providers/chat_list_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$chatListHash() => r'4e571726c39c5132094cd3faee09bbaf6c2fd6bd';
+
+/// See also [ChatList].
+@ProviderFor(ChatList)
+final chatListProvider =
+    AutoDisposeAsyncNotifierProvider<ChatList, List<ChatEntity>>.internal(
+  ChatList.new,
+  name: r'chatListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$chatListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ChatList = AutoDisposeAsyncNotifier<List<ChatEntity>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
