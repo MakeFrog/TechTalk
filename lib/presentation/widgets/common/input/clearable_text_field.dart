f1384ba7b30a4fc9aba239386d1325f2e0f595df
suneog <tnsdjr7@gmail.com>
2023-10-25 19:44:43 +0900

    fix: clear 아이콘 디자인 요소로 변경

--- lib/presentation/widgets/common/input/clearable_text_field.dart
+++ lib/presentation/widgets/common/input/clearable_text_field.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:techtalk/core/theme/extension/app_color.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:techtalk/core/core.dart';
 
 class ClearableTextField extends HookWidget {
   const ClearableTextField({
@@ -76,10 +76,9 @@ class ClearableTextField extends HookWidget {
           () {
             controller.clear();
           },
-      icon: FaIcon(
-        FontAwesomeIcons.solidCircleXmark,
-        size: 16.r,
-        color: AppColor.of.gray2,
+      icon: SvgPicture.asset(
+        Assets.iconsRoundedCloseThick,
+        width: 22.r,
       ),
     );
   }
