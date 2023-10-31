078062a303e991513a6780df9fa1589e58083a7c
XimYa <hj_lee_com@naver.com>
2023-10-31 13:03:57 +0900

    chore : 기타 수정사항

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -17,13 +17,18 @@ class Assets {
   static const String iconsArrowUp = 'assets/icons/arrow_up.svg';
   static const String iconsArrowUpThick = 'assets/icons/arrow_up_thick.svg';
   static const String iconsAvatar1 = 'assets/icons/avatar_1.svg';
+  static const String iconsBullet = 'assets/icons/bullet.svg';
   static const String iconsCamera = 'assets/icons/camera.svg';
   static const String iconsCheck = 'assets/icons/check.svg';
   static const String iconsCheckThick = 'assets/icons/check_thick.svg';
   static const String iconsClose = 'assets/icons/close.svg';
   static const String iconsCloseThick = 'assets/icons/close_thick.svg';
   static const String iconsCorrect = 'assets/icons/correct.svg';
+  static const String iconsCorrectIndicator =
+      'assets/icons/correct_indicator.svg';
   static const String iconsDataStructure = 'assets/icons/data_structure.svg';
+  static const String iconsExpansionArrowIndicator =
+      'assets/icons/expansion_arrow_indicator.svg';
   static const String iconsGoogleLogo = 'assets/icons/google_logo.svg';
   static const String iconsHome = 'assets/icons/home.svg';
   static const String iconsIncorrect = 'assets/icons/incorrect.svg';
@@ -59,6 +64,7 @@ class Assets {
   static const String iconsTalker = 'assets/icons/talker.svg';
   static const String iconsUser = 'assets/icons/user.svg';
   static const String iconsWarnning = 'assets/icons/warnning.svg';
+  static const String iconsWrongIndicator = 'assets/icons/wrong_indicator.svg';
   static const String imagesAvatar1 = 'assets/images/avatar_1.png';
   static const String imagesWelcomeTechtalk =
       'assets/images/welcome_techtalk.svg';
--- lib/presentation/widgets/common/common.dart
+++ lib/presentation/widgets/common/common.dart
@@ -1,3 +1,3 @@
+export 'box/empty_box.dart';
 export 'input/clearable_text_field.dart';
-export 'layout/empty_box.dart';
 export 'toast/app_toast.dart';
