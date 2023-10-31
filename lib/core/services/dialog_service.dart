44467c8d665e458bc4154f0e69afda2637c6cd89
XimYa <hj_lee_com@naver.com>
2023-10-31 12:56:01 +0900

    feature : dialogService 모듈 추가
    - 기존 ToastService와 유사하게 작동

---
+++ lib/core/services/dialog_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:techtalk/app/router/router.dart';
+
+class DialogService {
+  DialogService._();
+
+  static void show({
+    required Dialog dialog,
+  }) {
+    showDialog(
+        context: rootNavigatorKey.currentContext!, builder: (_) => dialog);
+  }
+}
