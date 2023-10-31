927faeb369d4b41d0fb9f1986f8bd86c7bc302bb
XimYa <hj_lee_com@naver.com>
2023-10-31 13:03:48 +0900

    feature  : di 로직 추가

--- lib/app/di/modules/chat_di.dart
+++ lib/app/di/modules/chat_di.dart
@@ -1,6 +1,7 @@
 import 'package:techtalk/app/di/feature_di_interface.dart';
 import 'package:techtalk/app/di/locator.dart';
 import 'package:techtalk/features/chat/chat.dart';
+import 'package:techtalk/features/chat/use_cases/get_interview_qna_list_use_case.dart';
 
 final class ChatDependencyInject extends FeatureDependencyInjection {
   @override
@@ -14,6 +15,7 @@ final class ChatDependencyInject extends FeatureDependencyInjection {
   @override
   void useCases() {
     locator.registerFactory(() => GetChatListUseCase(chatRepository));
+    locator.registerFactory(() => GetInterviewQnaListUseCase(chatRepository));
     locator.registerFactory(() => GetAnswerFeedbackUseCase());
   }
 }
