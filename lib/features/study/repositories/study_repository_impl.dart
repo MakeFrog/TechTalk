d6fd5aca0dbd139788191be9f6b2bce798f73630
suneog <tnsdjr7@gmail.com>
2023-11-09 12:12:52 +0900

    refactor: 학습 문제 조회 로직 리팩토링

--- lib/features/study/repositories/study_repository_impl.dart
+++ lib/features/study/repositories/study_repository_impl.dart
@@ -14,20 +14,22 @@ final class StudyRepositoryImpl implements StudyRepository {
 
   @override
   Future<Result<StudyQuestionListEntity>> getQuestionList(String techId) async {
-    StudyQuestionListModel questionsModel;
-
-    StudyQuestionListModel? cacheQuestionsModel =
+    StudyQuestionListModel? questionsModel =
         await _studyLocalDataSource.getQuestionList(techId);
 
-    if (cacheQuestionsModel != null) {
-      final lastUpdateDate =
-          await _studyRemoteDataSource.getLastQuestionsUpdateDate(techId);
-      if (lastUpdateDate.compareTo(cacheQuestionsModel.updateDate) == 0) {
-        questionsModel = cacheQuestionsModel;
-      } else {
-        questionsModel = await _studyRemoteDataSource.getQuestionList(techId);
-      }
-    } else {
+    // 캐시 데이터가 존재하지 않을때는 바로 원격 데이터를 요청, 반환
+    if (questionsModel == null) {
+      questionsModel = await _studyRemoteDataSource.getQuestionList(techId);
+
+      return Result.success(
+        StudyQuestionListEntity.fromModel(questionsModel),
+      );
+    }
+
+    // 업데이트 날짜를 비교하여 최신이 아니면 최신 데이터 조회,캐시 업데이트 후 반환
+    final lastUpdateDate =
+        await _studyRemoteDataSource.getLastQuestionsUpdateDate(techId);
+    if (lastUpdateDate.compareTo(questionsModel.updateDate) != 0) {
       questionsModel = await _studyRemoteDataSource.getQuestionList(techId);
     }
 
