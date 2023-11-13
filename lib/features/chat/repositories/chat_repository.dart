2b428adb92b6c9e24629a0fb1f197321c9a12641
XimYa <hj_lee_com@naver.com>
2023-11-14 06:07:21 +0900

    feature : 채팅 repository 메소드 추가 및 제거

--- lib/features/chat/repositories/chat_repository.dart
+++ lib/features/chat/repositories/chat_repository.dart
@@ -1,17 +1,16 @@
 import 'package:techtalk/core/utils/result.dart';
 import 'package:techtalk/features/chat/entities/chat_entity.dart';
-import 'package:techtalk/features/chat/entities/interview_qna_entity.dart';
+import 'package:techtalk/features/chat/entities/interview_question_entity.dart';
 
 abstract interface class ChatRepository {
   /// 채팅 리스트 호출
   Future<Result<List<ChatEntity>>> getChatList(String roomId);
 
-  /// 면접 질문 리스트 QnA 리스트 호출
-  Future<List<InterviewQnAEntity>> getInitialQuestions(String topicId);
-
-  /// 진행중인 문답 리스트 호출
-  Future<Result<List<InterviewQnAEntity>>> getOngoingQnaList(String roomId);
-
   /// 모범답안 리스트 호출
-  Future<Result<List<String>>> getIdealAnswers(String questionId);
+  Future<Result<List<String>>> getIdealAnswers(
+    InterviewQuestionEntity questionId,
+  );
+
+  /// 랜던 문제 호출
+  Future<Result<InterviewQuestionEntity>> getRandomQuestion(String categoryId);
 }
