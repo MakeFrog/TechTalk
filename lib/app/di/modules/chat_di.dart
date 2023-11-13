90d22d8e39700e854928c6f23ee3ba16b95a6f6c
XimYa <hj_lee_com@naver.com>
2023-11-14 06:07:58 +0900

    feature : chat dependency 로직 추가

--- lib/app/di/modules/chat_di.dart
+++ lib/app/di/modules/chat_di.dart
@@ -1,7 +1,9 @@
 import 'package:techtalk/app/di/feature_di_interface.dart';
 import 'package:techtalk/app/di/locator.dart';
 import 'package:techtalk/features/chat/chat.dart';
-import 'package:techtalk/features/chat/use_cases/get_interview_qna_list_use_case.dart';
+import 'package:techtalk/features/chat/use_cases/get_question_ideal_answers_use_case.dart';
+import 'package:techtalk/features/chat/use_cases/get_random_interview_question_use_case.dart';
+import 'package:techtalk/features/chat/use_cases/retrieve_qna_list_from_chat_list_use_case.dart';
 
 final class ChatDependencyInject extends FeatureDependencyInjection {
   @override
@@ -14,8 +16,13 @@ final class ChatDependencyInject extends FeatureDependencyInjection {
 
   @override
   void useCases() {
+    locator.registerFactory(() => RetrieveQnaListFromChatListUseCase());
     locator.registerFactory(() => GetChatListUseCase(chatRepository));
-    locator.registerFactory(() => GetInterviewQnaListUseCase(chatRepository));
+    // locator.registerFactory(() => GetInterviewQnaListUseCase(chatRepository));
+    locator
+        .registerFactory(() => GetQuestionIdealAnswersUseCase(chatRepository));
     locator.registerFactory(() => GetAnswerFeedbackUseCase());
+    locator.registerFactory(
+        () => GetRandomInterviewQuestionUseCase(chatRepository));
   }
 }
