48fb46e470b8ed326d28fc725a3f7f48e10d7447
XimYa <hj_lee_com@naver.com>
2023-11-14 06:05:04 +0900

    refactor : chat entity 종류 세분화
    - 기존 Received & Sent Entity 구조에서 채팅 타입에 따라 종류를 세분화함.
    
    - received 제거, guide, answer, qusetion 타입 추가

--- lib/features/chat/entities/sent_chat_entity.dart
+++ lib/features/chat/entities/sent_chat_entity.dart
@@ -5,17 +5,24 @@ import 'package:techtalk/features/chat/enums/chat_type.enum.dart';
 
 class SentChatEntity extends ChatEntity {
   final AnswerState answerState;
+  final String questionId;
 
   SentChatEntity({
     required String message,
     required this.answerState,
+    required this.questionId,
   }) : super(
           message: BehaviorSubject.seeded(message)..close(),
           type: ChatType.userReply,
           isStreamApplied: false,
         );
 
-  factory SentChatEntity.create({required String message}) => SentChatEntity(
+  factory SentChatEntity.initial({
+    required String message,
+    required String questionId,
+  }) =>
+      SentChatEntity(
+        questionId: questionId,
         message: message,
         answerState: AnswerState.loading,
       );
@@ -27,6 +34,7 @@ class SentChatEntity extends ChatEntity {
     return SentChatEntity(
       message: message ?? this.message.value,
       answerState: answerState ?? this.answerState,
+      questionId: questionId,
     );
   }
 }
