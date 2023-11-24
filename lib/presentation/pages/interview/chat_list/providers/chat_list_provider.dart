6e180b45b98af1502a9a0664447168039f80e7ef
XimYa <hj_lee_com@naver.com>
2023-11-25 05:18:22 +0900

    feature : 채팅 리스트 페이지 -> 채팅 페이지 라우팅 로직 고도화

--- lib/presentation/pages/interview/chat_list/providers/chat_list_provider.dart
+++ lib/presentation/pages/interview/chat_list/providers/chat_list_provider.dart
@@ -8,11 +8,24 @@ part 'chat_list_provider.g.dart';
 @riverpod
 class ChatList extends _$ChatList {
   @override
-  FutureOr<List<ChatRoomListItemEntity>> build() async {
+  FutureOr<List<ChatRoomEntity>> build() async {
     final response = await getChatListUseCase(InterviewTopic.swift);
     return response.fold(
       onSuccess: (chatList) {
         return chatList;
+      },
+      onFailure: (e) {
+        log(e.toString());
+        throw e;
+      },
+    );
+  }
+
+  Future<void> updateChatList() async {
+    final response = await getChatListUseCase(InterviewTopic.swift);
+    return response.fold(
+      onSuccess: (chatList) {
+        update((_) => chatList);
       },
       onFailure: (e) {
         log(e.toString());
--- lib/presentation/pages/interview/chat_list/providers/chat_list_provider.g.dart
+++ lib/presentation/pages/interview/chat_list/providers/chat_list_provider.g.dart
@@ -6,12 +6,12 @@ part of 'chat_list_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$chatListHash() => r'538f125e13d64ae67e7e88a8c9ce3148be5a2d6f';
+String _$chatListHash() => r'd4f8e43e2294b3c39c91a1981eb5c0e0b8885284';
 
 /// See also [ChatList].
 @ProviderFor(ChatList)
-final chatListProvider = AutoDisposeAsyncNotifierProvider<ChatList,
-    List<ChatRoomListItemEntity>>.internal(
+final chatListProvider =
+    AutoDisposeAsyncNotifierProvider<ChatList, List<ChatRoomEntity>>.internal(
   ChatList.new,
   name: r'chatListProvider',
   debugGetCreateSourceHash:
@@ -20,6 +20,6 @@ final chatListProvider = AutoDisposeAsyncNotifierProvider<ChatList,
   allTransitiveDependencies: null,
 );
 
-typedef _$ChatList = AutoDisposeAsyncNotifier<List<ChatRoomListItemEntity>>;
+typedef _$ChatList = AutoDisposeAsyncNotifier<List<ChatRoomEntity>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
