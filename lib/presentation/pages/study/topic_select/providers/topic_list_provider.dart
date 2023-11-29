32577c11ac7766a8ff6df643993af043f2257c04
XimYa <hj_lee_com@naver.com>
2023-11-29 10:41:15 +0900

    feature : 면접 문제 개수 선택 페이지 UI 및 관련 라우팅 로직 추가

--- lib/presentation/pages/study/topic_select/providers/topic_list_provider.dart
+++ lib/presentation/pages/study/topic_select/providers/topic_list_provider.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:techtalk/app/di/locator.dart';
+import 'package:techtalk/core/services/toast_service.dart';
 import 'package:techtalk/features/chat/chat.dart';
 import 'package:techtalk/features/interview/interview.dart';
+import 'package:techtalk/presentation/widgets/common/common.dart';
 
 part 'topic_list_provider.g.dart';
 
@@ -11,20 +15,30 @@ Future<Map<String, List<InterviewTopic>>> studyTopicList(
 ) async {
   final getInterviewTopicListUseCase = locator<GetInterviewTopicListUseCase>();
 
-  final topicList = List.of(await getInterviewTopicListUseCase());
-  topicList.sort(
-    (a, b) => a.category.text.compareTo(b.category.text),
+  final response = await getInterviewTopicListUseCase();
+
+  return response.fold(
+    onSuccess: (topicList) {
+      topicList.sort(
+        (a, b) => a.category.text.compareTo(b.category.text),
+      );
+
+      final resolvedTopicList = <String, List<InterviewTopic>>{};
+
+      for (final topic in topicList) {
+        if (resolvedTopicList.containsKey(topic.category.text)) {
+          resolvedTopicList[topic.category.text]!.add(topic);
+        } else {
+          resolvedTopicList[topic.category.text] = [topic];
+        }
+      }
+
+      return resolvedTopicList;
+    },
+    onFailure: (e) {
+      log(e.toString());
+      ToastService.show(CustomToast(message: e.toString()));
+      throw e;
+    },
   );
-
-  final resolvedTopicList = <String, List<InterviewTopic>>{};
-
-  for (final topic in topicList) {
-    if (resolvedTopicList.containsKey(topic.category.text)) {
-      resolvedTopicList[topic.category.text]!.add(topic);
-    } else {
-      resolvedTopicList[topic.category.text] = [topic];
-    }
-  }
-
-  return resolvedTopicList;
 }
