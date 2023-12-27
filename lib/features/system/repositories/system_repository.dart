d11ee6129a6df3f5780cf6f1fc7997074e1de2a9
suneog <tnsdjr7@gmail.com>
2023-12-27 16:06:33 +0900

    refactor: system feature 리팩토링

--- lib/features/system/repositories/system_repository.dart
+++ lib/features/system/repositories/system_repository.dart
@@ -1,5 +1,5 @@
 import 'package:techtalk/core/utils/result.dart';
-import 'package:techtalk/features/system/repositories/entities/version_entity.dart';
+import 'package:techtalk/features/system/entities/version_entity.dart';
 
 abstract interface class SystemRepository {
   Future<Result<VersionEntity>> getVersionInfo();
