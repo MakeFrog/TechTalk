9de9931b8cfda5249580ba0c623dad043ab0b39b
suneog <tnsdjr7@gmail.com>
2023-12-28 15:41:53 +0900

    fix: 면접 보기 클릭 시 진행중인 면접이 없다면 면접 시작하기 화면으로 이동

--- lib/presentation/pages/home/home_event.dart
+++ lib/presentation/pages/home/home_event.dart
@@ -1,6 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:techtalk/app/router/router.dart';
 import 'package:techtalk/features/topic/topic.dart';
+import 'package:techtalk/presentation/pages/interview/chat_list/providers/interview_rooms_provider.dart';
 
 mixin class HomeEvent {
   void onTapPracticalInterview() {
@@ -14,7 +15,12 @@ mixin class HomeEvent {
   void onTapGoToInterviewRoomListPage(
     WidgetRef ref, {
     required TopicEntity topic,
-  }) {
-    ChatListPageRoute(topic.id).push(rootNavigatorKey.currentContext!);
+  }) async {
+    final chatRooms = await ref.read(interviewRoomsProvider(topic.id).future);
+    if (chatRooms.isEmpty) {
+      QuestionCountSelectPageRoute($extra: topic).push(ref.context);
+    } else {
+      ChatListPageRoute(topic.id).push(rootNavigatorKey.currentContext!);
+    }
   }
 }
