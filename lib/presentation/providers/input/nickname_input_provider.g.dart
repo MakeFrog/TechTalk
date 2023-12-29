523abd6619d73eb30e12fb8a2402deb9bdd21c3b
XimYa <hj_lee_com@naver.com>
2023-12-29 15:20:23 +0900

    feature : 프로필 변경 페이지 UI 인터렉션 로직 추가

---
+++ lib/presentation/providers/input/nickname_input_provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nickname_input_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$nicknameInputHash() => r'287ce8b28910261d811e4f58d6e247f555fe032a';
+
+/// See also [NicknameInput].
+@ProviderFor(NicknameInput)
+final nicknameInputProvider =
+    AutoDisposeNotifierProvider<NicknameInput, String?>.internal(
+  NicknameInput.new,
+  name: r'nicknameInputProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$nicknameInputHash,
+  dependencies: <ProviderOrFamily>[userDataProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    userDataProvider,
+    ...?userDataProvider.allTransitiveDependencies
+  },
+);
+
+typedef _$NicknameInput = AutoDisposeNotifier<String?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
