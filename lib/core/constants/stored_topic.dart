0bd47b1101040add9ec4ced9946a9040284da880
XimYa <hj_lee_com@naver.com>
2024-01-17 22:20:36 +0900

    feature : 애니메이션 앱바 위젯 추가

---
+++ lib/core/constants/stored_topic.dart
@@ -0,0 +1,40 @@
+import 'dart:developer';
+
+import 'package:techtalk/features/topic/topic.dart';
+
+///
+/// '면접 주제' 리스트를 보관하는 클래스
+/// [Splash] 단계에서 데이터를 초기화됨.
+///
+class StoredTopics {
+  static final List<TopicEntity> list = [];
+
+  static Future<void> initializeTopics() async {
+    final response = getTopicsUseCase.call();
+
+    response.fold(
+      onSuccess: (topics) {
+        list.addAll(topics);
+      },
+      onFailure: (e) {
+        log(e.toString());
+      },
+    );
+  }
+
+  /// 특정 면접 주제 entity를 id 값을 기반으로 리턴
+  static TopicEntity getById(String id) {
+    return list.firstWhere(
+      (topic) => topic.id == id,
+      orElse: () => throw Exception('Unexpected Topic Id Value'),
+    );
+  }
+
+  static TopicEntity? getByIdOrNull(String? id) {
+    if (id == null) return null;
+    return list.firstWhere(
+      (topic) => topic.id == id,
+      orElse: () => throw Exception('Unexpected Topic Id Value'),
+    );
+  }
+}
---
+++ lib/presentation/widgets/common/app_bar/animated_app_bar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
+import 'package:techtalk/core/theme/extension/app_color.dart';
+import 'package:techtalk/core/theme/extension/app_text_style.dart';
+import 'package:techtalk/presentation/widgets/common/button/app_back_button.dart';
+
+///
+/// 스크롤 포지션에 따라
+/// 앱바 제목의 Opacity가 Animated되는 로직이 적용되어 있는 모듈
+///
+///
+
+class AnimatedAppBar extends HookWidget implements PreferredSizeWidget {
+  const AnimatedAppBar({
+    super.key,
+    required this.title,
+    required this.scrollController,
+    required this.opacityPosition,
+    bool? showBackBtn,
+    this.onBackBtnTapped,
+    this.actions,
+  }) : showBackBtn = showBackBtn ?? true;
+
+  final String title;
+  final bool showBackBtn;
+  final VoidCallback? onBackBtnTapped;
+  final ScrollController scrollController;
+  final double opacityPosition; // Opacity 변환 지점
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    final showTitle = useState(false);
+
+    scrollController.addListener(() {
+      final scrollPosition = scrollController.offset;
+
+      if (opacityPosition < scrollPosition && !showTitle.value) {
+        showTitle.value = true;
+        return;
+      }
+
+      if (opacityPosition >= scrollPosition && showTitle.value) {
+        showTitle.value = false;
+        return;
+      }
+    });
+
+    return AppBar(
+      titleSpacing: 0,
+      backgroundColor: AppColor.of.white,
+      centerTitle: false,
+      automaticallyImplyLeading: false,
+      leadingWidth: 56,
+      actions: actions,
+      title: AnimatedOpacity(
+        opacity: showTitle.value ? 1 : 0,
+        duration: const Duration(milliseconds: 400),
+        child: Text(
+          title,
+          style: AppTextStyle.headline2,
+        ),
+      ),
+      leading: showBackBtn
+          ? AppBackButton(
+              onBackBtnTapped: onBackBtnTapped ?? context.pop,
+            )
+          : null,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}
