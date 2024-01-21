928ed188d819c7ebf707a216ec1574a0cc478c45
XimYa <hj_lee_com@naver.com>
2024-01-21 23:36:30 +0900

    refactor : 인터뷰 진행 상태 provider 로직 리팩토링

--- lib/presentation/pages/interview/chat/providers/interview_progress_state_provider.dart
+++ lib/presentation/pages/interview/chat/providers/interview_progress_state_provider.dart
@@ -2,46 +2,43 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:techtalk/features/chat/chat.dart';
 import 'package:techtalk/presentation/pages/interview/chat/providers/chat_message_history_provider.dart';
 import 'package:techtalk/presentation/pages/interview/chat/providers/chat_qnas_provider.dart';
+import 'package:techtalk/presentation/pages/interview/chat/providers/selected_chat_room_provider.dart';
 
 part 'interview_progress_state_provider.g.dart';
 
-@Riverpod()
+@Riverpod(dependencies: [selectedChatRoom, ChatQnas, ChatMessageHistory])
 class InterviewProgressState extends _$InterviewProgressState {
   @override
-  InterviewProgress build(ChatRoomEntity room) {
-    final qnas = ref.watch(chatQnAsProvider(room)).valueOrNull;
+  InterviewProgress build() {
+    final progressState = ref.read(selectedChatRoomProvider).progressState;
 
-    if (qnas != null) {
-      final totalQuestionCount = qnas.length;
-      final completedQnAs = qnas.where(
-        (e) => e.hasUserResponded,
-      );
-
-      if (completedQnAs.length >= totalQuestionCount) {
-        return InterviewProgress.done;
-      }
-
-      final chatMessages =
-          ref.watch(chatMessageHistoryProvider(room)).valueOrNull;
-      if (chatMessages != null && chatMessages.isNotEmpty) {
-        final lastChat = chatMessages.first;
-        switch (lastChat.type) {
-          case ChatType.reply:
-          case ChatType.guide:
-          case ChatType.feedback:
-            state = InterviewProgress.interviewerReplying;
-          case ChatType.question:
-            lastChat.message.listen(
-              null,
-              onDone: () {
-                state = InterviewProgress.readyToAnswer;
-              },
-            );
-        }
-      }
+    if (progressState.isCompleted) {
+      return InterviewProgress.done;
+    } else {
+      _listenStateBasedOnChatHistory();
+      if (progressState.isInitial) return InterviewProgress.initial;
+      if (progressState.isOngoing) return InterviewProgress.readyToAnswer;
     }
 
-    return InterviewProgress.initial;
+    return InterviewProgress.readyToAnswer;
+  }
+
+  void _listenStateBasedOnChatHistory() {
+    ref.listen(chatMessageHistoryProvider, (_, chatHistory) {
+      final lastChat = chatHistory.value!.first;
+      switch (lastChat.type) {
+        case ChatType.guide:
+        case ChatType.question:
+          lastChat.message.listen(
+            null,
+            onDone: () {
+              state = InterviewProgress.readyToAnswer;
+            },
+          );
+        default:
+          state = InterviewProgress.interviewerReplying;
+      }
+    });
   }
 }
 
--- lib/presentation/pages/interview/chat/providers/interview_progress_state_provider.g.dart
+++ lib/presentation/pages/interview/chat/providers/interview_progress_state_provider.g.dart
@@ -7,170 +7,32 @@ part of 'interview_progress_state_provider.dart';
 // **************************************************************************
 
 String _$interviewProgressStateHash() =>
-    r'040860934703453d2e04e58a8eb5b64856539080';
-
-/// Copied from Dart SDK
-class _SystemHash {
-  _SystemHash._();
-
-  static int combine(int hash, int value) {
-    // ignore: parameter_assignments
-    hash = 0x1fffffff & (hash + value);
-    // ignore: parameter_assignments
-    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
-    return hash ^ (hash >> 6);
-  }
-
-  static int finish(int hash) {
-    // ignore: parameter_assignments
-    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
-    // ignore: parameter_assignments
-    hash = hash ^ (hash >> 11);
-    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
-  }
-}
-
-abstract class _$InterviewProgressState
-    extends BuildlessAutoDisposeNotifier<InterviewProgress> {
-  late final ChatRoomEntity room;
-
-  InterviewProgress build(
-    ChatRoomEntity room,
-  );
-}
+    r'8062410c88c88ca8a4fbc0aadc2ed13d08231d04';
 
 /// See also [InterviewProgressState].
 @ProviderFor(InterviewProgressState)
-const interviewProgressStateProvider = InterviewProgressStateFamily();
+final interviewProgressStateProvider = AutoDisposeNotifierProvider<
+    InterviewProgressState, InterviewProgress>.internal(
+  InterviewProgressState.new,
+  name: r'interviewProgressStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$interviewProgressStateHash,
+  dependencies: <ProviderOrFamily>[
+    selectedChatRoomProvider,
+    chatQnasProvider,
+    chatMessageHistoryProvider
+  ],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    selectedChatRoomProvider,
+    ...?selectedChatRoomProvider.allTransitiveDependencies,
+    chatQnasProvider,
+    ...?chatQnasProvider.allTransitiveDependencies,
+    chatMessageHistoryProvider,
+    ...?chatMessageHistoryProvider.allTransitiveDependencies
+  },
+);
 
-/// See also [InterviewProgressState].
-class InterviewProgressStateFamily extends Family<InterviewProgress> {
-  /// See also [InterviewProgressState].
-  const InterviewProgressStateFamily();
-
-  /// See also [InterviewProgressState].
-  InterviewProgressStateProvider call(
-    ChatRoomEntity room,
-  ) {
-    return InterviewProgressStateProvider(
-      room,
-    );
-  }
-
-  @override
-  InterviewProgressStateProvider getProviderOverride(
-    covariant InterviewProgressStateProvider provider,
-  ) {
-    return call(
-      provider.room,
-    );
-  }
-
-  static const Iterable<ProviderOrFamily>? _dependencies = null;
-
-  @override
-  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
-
-  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
-
-  @override
-  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
-      _allTransitiveDependencies;
-
-  @override
-  String? get name => r'interviewProgressStateProvider';
-}
-
-/// See also [InterviewProgressState].
-class InterviewProgressStateProvider extends AutoDisposeNotifierProviderImpl<
-    InterviewProgressState, InterviewProgress> {
-  /// See also [InterviewProgressState].
-  InterviewProgressStateProvider(
-    ChatRoomEntity room,
-  ) : this._internal(
-          () => InterviewProgressState()..room = room,
-          from: interviewProgressStateProvider,
-          name: r'interviewProgressStateProvider',
-          debugGetCreateSourceHash:
-              const bool.fromEnvironment('dart.vm.product')
-                  ? null
-                  : _$interviewProgressStateHash,
-          dependencies: InterviewProgressStateFamily._dependencies,
-          allTransitiveDependencies:
-              InterviewProgressStateFamily._allTransitiveDependencies,
-          room: room,
-        );
-
-  InterviewProgressStateProvider._internal(
-    super._createNotifier, {
-    required super.name,
-    required super.dependencies,
-    required super.allTransitiveDependencies,
-    required super.debugGetCreateSourceHash,
-    required super.from,
-    required this.room,
-  }) : super.internal();
-
-  final ChatRoomEntity room;
-
-  @override
-  InterviewProgress runNotifierBuild(
-    covariant InterviewProgressState notifier,
-  ) {
-    return notifier.build(
-      room,
-    );
-  }
-
-  @override
-  Override overrideWith(InterviewProgressState Function() create) {
-    return ProviderOverride(
-      origin: this,
-      override: InterviewProgressStateProvider._internal(
-        () => create()..room = room,
-        from: from,
-        name: null,
-        dependencies: null,
-        allTransitiveDependencies: null,
-        debugGetCreateSourceHash: null,
-        room: room,
-      ),
-    );
-  }
-
-  @override
-  AutoDisposeNotifierProviderElement<InterviewProgressState, InterviewProgress>
-      createElement() {
-    return _InterviewProgressStateProviderElement(this);
-  }
-
-  @override
-  bool operator ==(Object other) {
-    return other is InterviewProgressStateProvider && other.room == room;
-  }
-
-  @override
-  int get hashCode {
-    var hash = _SystemHash.combine(0, runtimeType.hashCode);
-    hash = _SystemHash.combine(hash, room.hashCode);
-
-    return _SystemHash.finish(hash);
-  }
-}
-
-mixin InterviewProgressStateRef
-    on AutoDisposeNotifierProviderRef<InterviewProgress> {
-  /// The parameter `room` of this provider.
-  ChatRoomEntity get room;
-}
-
-class _InterviewProgressStateProviderElement
-    extends AutoDisposeNotifierProviderElement<InterviewProgressState,
-        InterviewProgress> with InterviewProgressStateRef {
-  _InterviewProgressStateProviderElement(super.provider);
-
-  @override
-  ChatRoomEntity get room => (origin as InterviewProgressStateProvider).room;
-}
+typedef _$InterviewProgressState = AutoDisposeNotifier<InterviewProgress>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
