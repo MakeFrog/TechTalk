08cb9b1f00f4d736ad7a987a8a0ddf23b33435c8
XimYa <hj_lee_com@naver.com>
2024-01-29 00:27:55 +0900

    feature : InterviewProgress 에러 enum 값 추가

--- lib/features/chat/entities/enums/interview_progress.enum.dart
+++ lib/features/chat/entities/enums/interview_progress.enum.dart
@@ -2,13 +2,15 @@ enum InterviewProgress {
   initial,
   readyToAnswer,
   interviewerReplying,
-  done;
+  done,
+  error;
 
   String get fieldHintText => switch (this) {
         InterviewProgress.initial => '잠시만 기다려주세요',
         InterviewProgress.readyToAnswer => '답변을 입력해주세요',
         InterviewProgress.interviewerReplying => '잠시만 기다려주세요',
         InterviewProgress.done => '면접이 종료되었습니다',
+        InterviewProgress.error => '예상하지 못한 오류가 발생했어요'
       };
 
   bool get enableChat => switch (this) {
@@ -16,5 +18,6 @@ enum InterviewProgress {
         InterviewProgress.readyToAnswer => true,
         InterviewProgress.interviewerReplying => false,
         InterviewProgress.done => false,
+        InterviewProgress.error => false,
       };
 }
