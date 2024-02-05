9e465ae4ad786b371cd7c75a56213298d3b4eb3f
XimYa <hj_lee_com@naver.com>
2024-02-06 02:41:43 +0900

    refactor : chatpage 라우팅 방식 변경 (build -> buildPage)

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -328,8 +328,29 @@ class ChatPageRoute extends GoRouteData {
   final InterviewType type;
   final String roomId;
 
+  // @override
+  // Widget build(BuildContext context, GoRouterState state) {
+  //   return const ChatPage();
+  // }
+
   @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return const ChatPage();
+  Page<void> buildPage(BuildContext context, GoRouterState state) {
+    return CustomTransitionPage(
+      fullscreenDialog: true,
+      transitionsBuilder: (_, animation, __, child) {
+        var begin = const Offset(1.0, 0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+      child: const ChatPage(),
+    );
   }
 }
