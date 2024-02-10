991af1c4d4ff724dd49afc01dca377d9f3422f99
XimYa <hj_lee_com@naver.com>
2024-02-10 23:17:34 +0900

    feature : 정렬된 면접 주제 목록 provider 추가

---
+++ lib/presentation/providers/topic/sorted_topics_provider.dart
@@ -0,0 +1,41 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:techtalk/core/constants/stored_topic.dart';
+import 'package:techtalk/features/topic/repositories/entities/topic_entity.dart';
+import 'package:techtalk/presentation/providers/user/user_info_provider.dart';
+
+part 'sorted_topics_provider.g.dart';
+
+///
+/// 제공되는 전체 면접 주제(Topic) 리스트에서
+/// 2가지 기준으로 정렬 및 필터링된 리스트
+/// - 유저의 관심 스킬과 관련된 면접 주제를 리스트 맨 앞으로 위치
+/// - 현재 유효하지 않은 면접 주제는 리스트에서 제외
+///
+
+@riverpod
+class SortedTopics extends _$SortedTopics {
+  @override
+  List<TopicEntity> build() {
+    // final availableTopics = StoredTopics.list.where((e) => e.isAvailable);
+    final availableTopics = StoredTopics.list;
+    final storedTopicsByUser = ref
+        .read(userInfoProvider)
+        .requireValue!
+        .skills
+        .map(
+          (e) => StoredTopics.getBySkillId(e.id),
+        )
+        .toList();
+
+    final storedList = availableTopics
+        .where(storedTopicsByUser.contains)
+        .followedBy(
+          availableTopics.where(
+            (e) => !storedTopicsByUser.contains(e),
+          ),
+        )
+        .toList();
+
+    return storedList;
+  }
+}
---
+++ lib/presentation/providers/topic/sorted_topics_provider.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sorted_topics_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sortedTopicsHash() => r'07545f357bd32b5a9aecfa7a9b97a219da10a6ee';
+
+///
+/// 제공되는 전체 면접 주제(Topic) 리스트에서
+/// 2가지 기준으로 정렬 및 필터링된 리스트
+/// - 유저의 관심 스킬과 관련된 면접 주제를 리스트 맨 앞으로 위치
+/// - 현재 유효하지 않은 면접 주제는 리스트에서 제외
+///
+///
+/// Copied from [SortedTopics].
+@ProviderFor(SortedTopics)
+final sortedTopicsProvider =
+    AutoDisposeNotifierProvider<SortedTopics, List<TopicEntity>>.internal(
+  SortedTopics.new,
+  name: r'sortedTopicsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$sortedTopicsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SortedTopics = AutoDisposeNotifier<List<TopicEntity>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
