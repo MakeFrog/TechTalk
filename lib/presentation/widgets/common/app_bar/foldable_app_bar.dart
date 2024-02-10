9e135e4d875aa9248ec1dd6aac7f4937d92c0300
XimYa <hj_lee_com@naver.com>
2024-02-11 01:50:56 +0900

    feature : foldable 애니메이션이 적용된 앱바 모듈 추가

---
+++ lib/presentation/widgets/common/app_bar/foldable_app_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:techtalk/core/theme/extension/app_text_style.dart';
+
+class FoldableAppBar extends HookWidget implements PreferredSizeWidget {
+  const FoldableAppBar({
+    super.key,
+    required this.title,
+    required this.scrollController,
+    required this.animatedPosition,
+  });
+
+  final String title;
+  final ScrollController scrollController;
+  final double animatedPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(56),
+      child: HookBuilder(
+        builder: (context) {
+          final isFold = useState(false);
+
+          scrollController.addListener(() {
+            final scrollPosition = scrollController.offset;
+
+            if (animatedPosition < scrollPosition && !isFold.value) {
+              isFold.value = true;
+              return;
+            }
+
+            if (animatedPosition >= scrollPosition && isFold.value) {
+              isFold.value = false;
+              return;
+            }
+          });
+          return AnimatedContainer(
+            height: isFold.value ? 0 : 56,
+            duration: const Duration(milliseconds: 200),
+            child: AnimatedOpacity(
+              opacity: isFold.value ? 0 : 1,
+              duration: const Duration(milliseconds: 200),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    title,
+                    style: AppTextStyle.headline2,
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}
