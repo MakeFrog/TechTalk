564bb17825b8e390ff9051d0827f9d185275eccb
XimYa <hj_lee_com@naver.com>
2024-02-17 10:35:55 +0900

    chore : 기타 수정사항

--- lib/presentation/pages/wrong_answer_note/local_widgets/wrong_answer_header.dart
+++ lib/presentation/pages/wrong_answer_note/local_widgets/wrong_answer_header.dart
@@ -17,91 +17,91 @@ class _WrongAnswerHeader extends HookWidget
             builder: (context) {
               final isFold = useState(false);
 
-              scrollController(ref).addListener(() {
-                final scrollPosition = scrollController(ref).offset;
+              scrollController(ref).addListener(
+                () {
+                  final scrollPosition = scrollController(ref).offset;
 
-                if (WrongAnswerNoteScrollController.animatedOffset <
-                        scrollPosition &&
-                    !isFold.value) {
-                  isFold.value = true;
-                  return;
-                }
+                  if (WrongAnswerNoteScrollController.animatedOffset <
+                          scrollPosition &&
+                      !isFold.value) {
+                    isFold.value = true;
+                    return;
+                  }
 
-                if (WrongAnswerNoteScrollController.animatedOffset >=
-                        scrollPosition &&
-                    isFold.value) {
-                  isFold.value = false;
-                  return;
-                }
-              });
+                  if (WrongAnswerNoteScrollController.animatedOffset >=
+                          scrollPosition &&
+                      isFold.value) {
+                    isFold.value = false;
+                    return;
+                  }
+                },
+              );
               return AnimatedContainer(
                 height: isFold.value ? 0 : 112,
                 duration: const Duration(milliseconds: 200),
                 child: AnimatedOpacity(
                   opacity: isFold.value ? 0 : 1,
                   duration: const Duration(milliseconds: 200),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          top: 0,
-                          child: Container(
-                            height: 56,
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '오답노트',
-                              style: AppTextStyle.headline2,
-                              textAlign: TextAlign.start,
-                            ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 16),
+                          height: 56,
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            '오답노트',
+                            style: AppTextStyle.headline2,
+                            textAlign: TextAlign.start,
                           ),
                         ),
-                        Positioned(
-                          bottom: 12,
-                          left: 0,
-                          right: 0,
-                          child: SizedBox(
-                            height: 32,
-                            child: ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: userTopicRecords(ref).length,
-                              separatorBuilder: (context, index) =>
-                                  const Gap(8),
-                              itemBuilder: (context, index) {
-                                final topic = userTopicRecords(ref)[index];
-                                final isSelected =
-                                    topic.id == selectedTopic(ref)!.id;
+                      ),
+                      Positioned(
+                        bottom: 12,
+                        left: 0,
+                        right: 0,
+                        child: SizedBox(
+                          height: 32,
+                          child: ListView.separated(
+                            padding: const EdgeInsets.only(left: 12),
+                            scrollDirection: Axis.horizontal,
+                            itemCount: userTopicRecords(ref).length,
+                            separatorBuilder: (context, index) => const Gap(8),
+                            itemBuilder: (context, index) {
+                              final topic = userTopicRecords(ref)[index];
+                              final isSelected =
+                                  topic.id == selectedTopic(ref)!.id;
 
-                                return ChoiceChip(
-                                  showCheckmark: false,
-                                  selected: isSelected,
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 12,
-                                    vertical: 6,
-                                  ),
-                                  backgroundColor: AppColor.of.background1,
-                                  selectedColor: AppColor.of.brand2,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  labelStyle: AppTextStyle.body1.copyWith(
-                                    color: isSelected
-                                        ? Colors.white
-                                        : AppColor.of.gray3,
-                                  ),
-                                  side: BorderSide.none,
-                                  onSelected: (value) => onTapTopicChip(
-                                    ref,
-                                    topic,
-                                  ),
-                                  label: Text(topic.text),
-                                );
-                              },
-                            ),
+                              return ChoiceChip(
+                                showCheckmark: false,
+                                selected: isSelected,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 12,
+                                  vertical: 6,
+                                ),
+                                backgroundColor: AppColor.of.background1,
+                                selectedColor: AppColor.of.brand2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                labelStyle: AppTextStyle.body1.copyWith(
+                                  color: isSelected
+                                      ? Colors.white
+                                      : AppColor.of.gray3,
+                                ),
+                                side: BorderSide.none,
+                                onSelected: (value) => onTapTopicChip(
+                                  ref,
+                                  topic,
+                                ),
+                                label: Text(topic.text),
+                              );
+                            },
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               );
