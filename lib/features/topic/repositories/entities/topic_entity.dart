313356e806a8c330c1d112f9d1af1261ad0f936d
XimYa <hj_lee_com@naver.com>
2024-02-26 00:07:58 +0900

    chore : 기타 수정사항

--- lib/features/topic/repositories/entities/topic_entity.dart
+++ lib/features/topic/repositories/entities/topic_entity.dart
@@ -18,58 +18,70 @@ class TopicEntity {
     required this.updatedAt,
   });
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'text': this.text,
+      'imageUrl': this.imageUrl,
+      'skillIds': this.skillIds,
+      'categoryId': this.categoryId,
+      'isAvailable': this.isAvailable,
+      'updatedAt': this.updatedAt,
+    };
+  }
+
+  factory TopicEntity.fromMap(Map<String, dynamic> map) {
+    return TopicEntity(
+      id: map['id'] as String,
+      text: map['text'] as String,
+      imageUrl: map['imageUrl'] as String,
+      skillIds: map['skillIds'] as List<String>,
+      categoryId: map['categoryId'] as String,
+      isAvailable: map['isAvailable'] as bool,
+      updatedAt: map['updatedAt'] as DateTime,
+    );
+  }
+
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      (other is TopicEntity &&
+      other is TopicEntity &&
           runtimeType == other.runtimeType &&
           id == other.id &&
           text == other.text &&
           imageUrl == other.imageUrl &&
+          skillIds == other.skillIds &&
           categoryId == other.categoryId &&
           isAvailable == other.isAvailable &&
-          updatedAt == other.updatedAt);
+          updatedAt == other.updatedAt;
 
   @override
   int get hashCode =>
       id.hashCode ^
       text.hashCode ^
       imageUrl.hashCode ^
+      skillIds.hashCode ^
       categoryId.hashCode ^
       isAvailable.hashCode ^
       updatedAt.hashCode;
-
-  @override
-  String toString() {
-    return 'TopicEntity{' +
-        ' id: $id,' +
-        ' text: $text,' +
-        ' imageUrl: $imageUrl,' +
-        ' categoryId: $categoryId,' +
-        ' isAvailable: $isAvailable,' +
-        ' updatedAt: $updatedAt,' +
-        '}';
-  }
 
   TopicEntity copyWith({
     String? id,
     String? text,
     String? imageUrl,
+    List<String>? skillIds,
     String? categoryId,
     bool? isAvailable,
     DateTime? updatedAt,
-    List<String>? skillIds,
   }) {
     return TopicEntity(
       id: id ?? this.id,
       text: text ?? this.text,
       imageUrl: imageUrl ?? this.imageUrl,
+      skillIds: skillIds ?? this.skillIds,
       categoryId: categoryId ?? this.categoryId,
       isAvailable: isAvailable ?? this.isAvailable,
       updatedAt: updatedAt ?? this.updatedAt,
-      skillIds: skillIds ?? this.skillIds,
     );
   }
-
-//</editor-fold>
 }
