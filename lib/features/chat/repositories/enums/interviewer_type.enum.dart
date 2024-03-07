dbea068c1a92a7f6df9393903a9c138d88bd5704
XimYa <hj_lee_com@naver.com>
2024-03-07 21:08:44 +0900

    chore : interviewertype enum 정보 수정

--- lib/features/chat/repositories/enums/interviewer_type.enum.dart
+++ lib/features/chat/repositories/enums/interviewer_type.enum.dart
@@ -6,10 +6,11 @@ enum Interviewer {
   blueTilde('blueTilde', '비트 단위 부정 면접관', Assets.characterBlue01),
   bluePlus('bluePlus', '증가 연산 면접관', Assets.characterBlue02),
   blueEqual('blueEqual', '동등 연산 면접관', Assets.characterBlue03),
-  blueComment('blueEqual', '주석 면접관', Assets.characterBlue04),
-  blueStar('blueEqual', '거듭제곱 면접관', Assets.characterBlue05),
-  blueString1('blueEqual', '문자열 면접관', Assets.characterBlue06),
-  blueString2('blueEqual', '문자열 면접관', Assets.characterBlue07),
+  blueComment('blueComment', '주석 면접관', Assets.characterBlue04),
+  blueStar('blueStar', '거듭제곱 면접관', Assets.characterBlue05),
+  blueString1('blueString1', '문자열 면접관', Assets.characterBlue06),
+  blueString2('blueString2', '문자열 면접관', Assets.characterBlue07),
+
   greenTilde('greenTilde', '비트 단위 부정 면접관', Assets.characterGreen01),
   greenPlus('greenPlus', '증가 연산 면접관', Assets.characterGreen02),
   greenEqual('greenEqual', '동등 연산 면접관', Assets.characterGreen03),
@@ -17,6 +18,7 @@ enum Interviewer {
   greenStar('greenStar', '거듭제곱 면접관', Assets.characterGreen05),
   greenString1('greenString1', '문자열 면접관', Assets.characterGreen06),
   greenString2('greenString2', '문자열 면접관', Assets.characterGreen07),
+
   purpleTilde('purpleTilde', '비트 단위 부정 면접관', Assets.characterPurple01),
   purplePlus('purplePlus', '증가 연산 면접관', Assets.characterPurple02),
   purpleEqual('purpleEqual', '동등 연산자 면접관', Assets.characterPurple03),
@@ -24,6 +26,7 @@ enum Interviewer {
   purpleStar('purpleStar', '거듭제곱 면접관', Assets.characterPurple05),
   purpleString1('purpleString1', '문자열 면접관', Assets.characterPurple06),
   purpleString2('purpleString2', '문자열 면접관', Assets.characterPurple07),
+
   redTilde('redTilde', '비트 단위 부정 면접관', Assets.characterRed01),
   redPlus('redPlus', '증가 연산 면접관', Assets.characterRed02),
   redEqual('redEqual', '동등 연산 면접관', Assets.characterRed03),
