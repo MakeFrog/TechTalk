ec7ba7d2aea983a6c4333e511f5adcf55780b1e1
yundal <soneymarr@gmail.com>
2024-09-04 16:04:15 +0900

    feature : Speech-to-Text 기능 추가 및 음성 모드 UI 구현
    - speech_to_text 패키지 추가
    - iOS 마이크 접근 허용권한 설정
    - [onMicBtnTapped] : 마이크 버튼 클릭시 이벤트 추가
    - [InterviewTabView] : 마이크 버튼 클릭시 음성 모드 UI 추가

--- lib/presentation/pages/interview/chat/widgets/interview_tab_view.dart
+++ lib/presentation/pages/interview/chat/widgets/interview_tab_view.dart
@@ -1,12 +1,16 @@
+// ignore_for_file: avoid_print
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:techtalk/app/style/index.dart';
 import 'package:techtalk/core/index.dart';
 import 'package:techtalk/features/chat/chat.dart';
+import 'package:techtalk/presentation/app.dart';
 import 'package:techtalk/presentation/pages/interview/chat/chat_event.dart';
 import 'package:techtalk/presentation/pages/interview/chat/chat_state.dart';
 import 'package:techtalk/presentation/pages/interview/chat/providers/chat_message_history_provider.dart';
@@ -21,6 +25,8 @@ class InterviewTabView extends HookConsumerWidget with ChatState, ChatEvent {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isSpeechMode = useState(false);
+
     useAutomaticKeepAlive();
 
     return Column(
@@ -36,32 +42,38 @@ class InterviewTabView extends HookConsumerWidget with ChatState, ChatEvent {
                     return Align(
                       alignment: Alignment.topCenter,
                       child: ListView.separated(
-                          controller: chatScrollController(ref),
-                          shrinkWrap: true,
-                          reverse: true,
-                          padding: const EdgeInsets.only(top: 24, bottom: 20) +
-                              const EdgeInsets.symmetric(horizontal: 12),
-                          separatorBuilder: (_, __) => const Gap(8),
-                          itemCount: ref.watch(chatMessageHistoryProvider
-                              .select((value) => value.value?.length ?? 0)),
-                          itemBuilder: (context, index) {
-                            return Bubble(
-                              chat: chatMessageHistory(ref)[index],
-                              isLatestReceivedChatInEachSection: ref
-                                  .watch(chatMessageHistoryProvider.notifier)
-                                  .isLastReceivedChatInEachQuestion(
-                                      index: index),
-                              interviewer: interviewer(ref),
-                              onReportBtnTapped: () {
-                                onReportBtnTapped(ref, index: index);
-                              },
-                            );
-                          }),
+                        controller: chatScrollController(ref),
+                        shrinkWrap: true,
+                        reverse: true,
+                        padding: const EdgeInsets.only(top: 24, bottom: 20) +
+                            const EdgeInsets.symmetric(horizontal: 12),
+                        separatorBuilder: (_, __) => const Gap(8),
+                        itemCount: ref.watch(
+                          chatMessageHistoryProvider
+                              .select((value) => value.value?.length ?? 0),
+                        ),
+                        itemBuilder: (context, index) {
+                          return Bubble(
+                            chat: chatMessageHistory(ref)[index],
+                            isLatestReceivedChatInEachSection: ref
+                                .watch(chatMessageHistoryProvider.notifier)
+                                .isLastReceivedChatInEachQuestion(
+                                  index: index,
+                                ),
+                            interviewer: interviewer(ref),
+                            onReportBtnTapped: () {
+                              onReportBtnTapped(ref, index: index);
+                            },
+                          );
+                        },
+                      ),
                     );
                   },
                   error: (e, __) => const Center(
                     child: ExceptionIndicator(
-                        subTitle: '다시 시도해주세요', title: '채팅 내역을 불러오지 못했어요.'),
+                      subTitle: '다시 시도해주세요',
+                      title: '채팅 내역을 불러오지 못했어요.',
+                    ),
                   ),
                   loading: () =>
                       const Center(child: CircularProgressIndicator()),
@@ -71,17 +83,25 @@ class InterviewTabView extends HookConsumerWidget with ChatState, ChatEvent {
           ),
         ),
 
+        ///
         /// 하단 입력창
-        const _BottomInputField(),
+        /// 두 개의 AnimatedSlide를 사용하여 각각의 위젯을 슬라이드 전환
+        ///
+
+        if (isSpeechMode.value)
+          _BottomSpeechToTextField(isSpeechMode: isSpeechMode)
+        else
+          _BottomInputField(isSpeechMode: isSpeechMode),
       ],
     );
   }
 }
 
 class _BottomInputField extends HookConsumerWidget with ChatState, ChatEvent {
-  const _BottomInputField({
-    Key? key,
-  }) : super(key: key);
+  final ValueNotifier<bool> isSpeechMode;
+
+  const _BottomInputField({Key? key, required this.isSpeechMode})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -94,9 +114,10 @@ class _BottomInputField extends HookConsumerWidget with ChatState, ChatEvent {
         horizontal: 12,
       ),
       child: chatAsyncAdapterValue(ref).when(
-        data: (_) => _buildTextField(progressState(ref)),
-        error: (_, __) => _buildTextField(InterviewProgress.error),
-        loading: () => _buildTextField(InterviewProgress.initial),
+        data: (_) => _buildTextField(progressState(ref), isSpeechMode),
+        error: (_, __) =>
+            _buildTextField(InterviewProgress.error, isSpeechMode),
+        loading: () => _buildTextField(InterviewProgress.initial, isSpeechMode),
       ),
     );
   }
@@ -104,7 +125,10 @@ class _BottomInputField extends HookConsumerWidget with ChatState, ChatEvent {
   ///
   /// 입력폼
   ///
-  Widget _buildTextField(InterviewProgress progressState) {
+  Widget _buildTextField(
+    InterviewProgress progressState,
+    ValueNotifier<bool> isSpeechMode,
+  ) {
     return HookBuilder(
       builder: (context) {
         /// 하이라이트(마이크 기능 알림) 효과 노출 여부
@@ -176,12 +200,14 @@ class _BottomInputField extends HookConsumerWidget with ChatState, ChatEvent {
               duration: expandInOutDuration,
               child: AnimatedOpacity(
                 duration: const Duration(milliseconds: 120),
-                opacity: isFieldFocused ? 0 : 1,
+                opacity: isFieldFocused || isSpeechMode.value ? 0 : 1,
                 child: ShrinkGestureView(
                   onTap: () {
                     /// TODO
                     /// 윤수님 여기에 음성 인식 활성화 UI를 노출하는 로직을 연동해주시면됩니다!
-                    onMicBtnTapped();
+                    /// onMicBtnTapped(isSpeechMode);
+                    isSpeechMode.value = !isSpeechMode.value;
+                    print('현재 SpeechMode인가? : ${isSpeechMode.value}');
                   },
                   borderRadius: BorderRadius.circular(22),
                   child: Container(
@@ -258,6 +284,203 @@ class _BottomInputField extends HookConsumerWidget with ChatState, ChatEvent {
           ],
         );
       },
+    );
+  }
+}
+
+class _BottomSpeechToTextField extends HookConsumerWidget
+    with ChatState, ChatEvent {
+  final ValueNotifier<bool> isSpeechMode;
+
+  const _BottomSpeechToTextField({Key? key, required this.isSpeechMode})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // SpeechToText 객체 생성
+    final speechToText = useMemoized(stt.SpeechToText.new, []);
+    final isListening = useState(false);
+    final recognizedText = useState('');
+
+    // SpeechToText 초기화 및 리스닝 상태 처리
+    useEffect(() {
+      Future<void> initSpeech() async {
+        bool available = await speechToText.initialize(
+          onStatus: (status) => print('Speech status: $status'),
+          onError: (error) => print('Speech error: $error'),
+        );
+
+        if (available) {
+          print('초기화됨');
+        } else {
+          print('사용 불가능');
+        }
+      }
+
+      initSpeech();
+      return null;
+    });
+
+    // 음성 인식을 시작하는 함수
+    void startListening() {
+      speechToText.listen(
+        onResult: (result) {
+          recognizedText.value = result.recognizedWords;
+          print('Recognized words: ${recognizedText.value}');
+        },
+      );
+      isListening.value = true;
+    }
+
+    // 음성 인식을 중지하는 함수
+    void stopListening() {
+      speechToText.stop();
+      isListening.value = false;
+    }
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 12,
+      ),
+      child: Column(
+        children: [
+          if (!isListening.value && recognizedText.value.isNotEmpty)
+          _buildRecognizedText(recognizedText.value),
+          _buildSpeechField(
+            isSpeechMode,
+            isListening.value,
+            recognizedText.value,
+            startListening,
+            stopListening,
+          ),
+        ],
+      ),
+    );
+  }
+
+  ///
+  /// speech to text의 결과물을 보여줌
+  ///
+
+Widget _buildRecognizedText(String recognizedText) {
+  return Container(
+    width: double.infinity,
+    margin: const EdgeInsets.only(right: 12, left: 12),
+    padding: const EdgeInsets.all(12.0),
+    decoration: BoxDecoration(
+      color: AppColor.of.background1,
+      borderRadius: BorderRadius.circular(8.0),
+    ),
+    child: ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxHeight: 100.0, // 4줄에 해당하는 최대 높이를 설정 (줄바꿈에 따라 조정 가능)
+      ),
+      child: SingleChildScrollView(
+        child: Text(
+          recognizedText,
+          style: TextStyle(
+            fontSize: 16.0,
+            color: AppColor.of.black,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
+  ///
+  /// 음성 모드 입력 폼
+  ///
+  Widget _buildSpeechField(
+    ValueNotifier<bool> isSpeechMode,
+    bool isListening,
+    String recognizedText,
+    VoidCallback startListening,
+    VoidCallback stopListening,
+  ) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 36),
+      height: 143,
+      child: Stack(
+        children: [
+          // 음성 인식 버튼
+          Align(
+            alignment: Alignment.topCenter,
+            child: GestureDetector(
+              onTap: isListening ? stopListening : startListening,
+              child: Container(
+                width: 112,
+                height: 112,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColor.of.blue1,
+                ),
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 44,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      size: 44,
+                      isListening ? Icons.record_voice_over : Icons.mic,
+                      color: AppColor.of.brand2,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // 타이핑 모드, 녹음 취소 버튼
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 36),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // 타이핑 모드로 전환
+                  GestureDetector(
+                    onTap: () {
+                      isSpeechMode.value = !isSpeechMode.value;
+                      print('현재 SpeechMode인가? : ${isSpeechMode.value}');
+                    },
+                    child: CircleAvatar(
+                      radius: 24,
+                      backgroundColor: AppColor.of.blue1,
+                      child: Icon(
+                        Icons.cloud,
+                        color: AppColor.of.brand2,
+                      ),
+                    ),
+                  ),
+
+                  // UI 레이아웃을 위한 Gap 설정
+                  const Gap(48),
+
+                  // 녹음 취소
+                  GestureDetector(
+                    onTap: () {
+                      stopListening();
+                      print('현재 SpeechMode인가? : ${isSpeechMode.value}');
+                    },
+                    child: CircleAvatar(
+                      radius: 24,
+                      backgroundColor: AppColor.of.background1,
+                      child: Icon(
+                        Icons.close,
+                        color: AppColor.of.gray3,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
