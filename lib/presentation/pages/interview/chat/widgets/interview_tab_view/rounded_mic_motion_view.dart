db5c340e640fac8e6fa46970f1196033aca64925
XimYa <hj_lee_com@naver.com>
2024-09-22 21:56:24 +0900

    chore : 불필요 import문 제거

--- lib/presentation/pages/interview/chat/widgets/interview_tab_view/rounded_mic_motion_view.dart
+++ lib/presentation/pages/interview/chat/widgets/interview_tab_view/rounded_mic_motion_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:techtalk/app/style/app_color.dart';
 import 'package:techtalk/presentation/pages/interview/chat/constant/recrod_progress_state.dart';
 import 'package:techtalk/presentation/pages/interview/chat/providers/speech_to_text_provider.dart';
