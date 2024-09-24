dab24aa2ef69a8435cf1f4557dbbed8cadf065dd
XimYa <hj_lee_com@naver.com>
2024-09-24 23:08:51 +0900

    feature : 음성 인식 실행중에 보여지는 원형 애니메이션 적용

--- lib/presentation/pages/interview/chat/widgets/interview_tab_view/rounded_mic_motion_view.dart
+++ lib/presentation/pages/interview/chat/widgets/interview_tab_view/rounded_mic_motion_view.dart
@@ -15,87 +15,137 @@ class RoundedMicMotionView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state =
         ref.watch(speechToTextProvider.select((c) => c.progressState));
-    return Container(
-      height: 112,
-      width: 112,
-      decoration: BoxDecoration(
-        color: AppColor.of.blue1,
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: Container(
-          height: 88,
-          width: 88,
-          decoration: BoxDecoration(
-            color: state.color,
-            boxShadow: const [
-              BoxShadow(
-                blurRadius: 12,
-                offset: Offset(0, 2),
-                color: Color.fromRGBO(217, 200, 239, 59),
-              )
-            ],
-            shape: BoxShape.circle,
-          ),
-          child: SizedBox(
-            child: Center(
-              child: Builder(
-                builder: (context) {
-                  switch (state) {
-                    case RecordProgressState.initial:
-                      return SvgPicture.asset(
-                        state.iconPath,
-                        height: 40,
-                        width: 40,
-                        colorFilter: ColorFilter.mode(
-                          AppColor.of.brand3,
-                          BlendMode.srcIn,
-                        ),
-                      );
-                    case RecordProgressState.ready ||
-                          RecordProgressState.onProgress:
-                      return HookConsumer(
-                        builder: (context, ref, _) {
-                          // AnimationController 설정
-                          final animationController = useAnimationController(
-                            duration: const Duration(milliseconds: 1650),
-                          );
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        /// 메인 녹음 버튼 Background / 애니메이션을 조건에 따라 실행
+        Positioned(
+          child: HookBuilder(
+            builder: (context) {
+              final bgAnimationController = useAnimationController(
+                duration: const Duration(milliseconds: 1000),
+                vsync: useSingleTickerProvider(),
+              );
 
-                          useEffect(() {
-                            animationController.repeat();
-                            return null;
-                          }, []);
+              final scale = Tween<double>(begin: 1, end: 1.1464).animate(
+                CurvedAnimation(
+                  parent: bgAnimationController,
+                  curve: Curves.easeInOut,
+                ),
+              );
 
-                          return StaggeredDotsWave(
-                            size: 24,
-                            color: AppColor.of.white,
-                            controller: animationController,
-                          );
-                        },
-                      );
-                    case RecordProgressState.loading:
-                      return HorizontalRotatingDots(
-                        color: AppColor.of.white,
-                        size: 30,
-                      );
+              if (state.isOnProgress && !bgAnimationController.isAnimating) {
+                bgAnimationController.repeat(reverse: true);
+              } else {
+                if (bgAnimationController.isAnimating) {
+                  Future.microtask(() async {
+                    // 원래 크기로 돌아오는것을 기다린 이후 애니메이션 멈춤
+                    await bgAnimationController.reverse();
+                    bgAnimationController.stop();
+                  });
+                }
+              }
 
-                    default:
-                      return SvgPicture.asset(
-                        state.iconPath,
-                        height: 40,
-                        width: 40,
-                        colorFilter: ColorFilter.mode(
-                          AppColor.of.brand3,
-                          BlendMode.srcIn,
-                        ),
-                      );
-                  }
+              return AnimatedBuilder(
+                animation: bgAnimationController,
+                builder: (BuildContext context, Widget? child) {
+                  return Transform.scale(
+                    scale: scale.value,
+                    child: Container(
+                      height: 120, // 원하는 최종 높이 설정
+                      width: 120, // 원하는 최종 너비 설정
+                      decoration: BoxDecoration(
+                        color: AppColor.of.blue1, // 원하는 색상으로 변경
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  );
                 },
+              );
+            },
+          ),
+        ),
+        SizedBox(
+          height: 112,
+          width: 112,
+          child: Center(
+            child: Container(
+              height: 88,
+              width: 88,
+              decoration: BoxDecoration(
+                color: state.color,
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 12,
+                    offset: Offset(0, 2),
+                    color: Color.fromRGBO(217, 200, 239, 59),
+                  )
+                ],
+                shape: BoxShape.circle,
+              ),
+              child: SizedBox(
+                child: Center(
+                  child: Builder(
+                    builder: (context) {
+                      switch (state) {
+                        case RecordProgressState.initial:
+                          return SvgPicture.asset(
+                            state.iconPath,
+                            height: 40,
+                            width: 40,
+                            colorFilter: ColorFilter.mode(
+                              AppColor.of.brand3,
+                              BlendMode.srcIn,
+                            ),
+                          );
+                        case RecordProgressState.ready ||
+                              RecordProgressState.onProgress:
+                          return HookConsumer(
+                            builder: (context, ref, _) {
+                              // AnimationController 설정
+                              final animationController =
+                                  useAnimationController(
+                                duration: const Duration(milliseconds: 1650),
+                              );
+
+                              useEffect(() {
+                                animationController.repeat();
+                                return null;
+                              }, []);
+
+                              return StaggeredDotsWave(
+                                size: 24,
+                                color: AppColor.of.white,
+                                controller: animationController,
+                              );
+                            },
+                          );
+                        case RecordProgressState.loading:
+                          return HorizontalRotatingDots(
+                            color: AppColor.of.white,
+                            size: 30,
+                          );
+
+                        default:
+                          return SvgPicture.asset(
+                            state.iconPath,
+                            height: 40,
+                            width: 40,
+                            colorFilter: ColorFilter.mode(
+                              AppColor.of.brand3,
+                              BlendMode.srcIn,
+                            ),
+                          );
+                      }
+                    },
+                  ),
+                ),
               ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
