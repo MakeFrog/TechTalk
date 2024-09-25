f139a89b2f27fe0dc8f09f143a8d15a401b98901
yundal <soneymarr@gmail.com>
2024-09-25 12:49:36 +0900

    chore : icon asset 추가

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -30,13 +30,13 @@ class Assets {
   static const String characterRed05 = 'assets/character/red_05.svg';
   static const String characterRed06 = 'assets/character/red_06.svg';
   static const String characterRed07 = 'assets/character/red_07.svg';
+  static const String followUpQuestion = 'assets/icons/follow_up_question.svg';
   static const String iconsArrowDown = 'assets/icons/arrow_down.svg';
   static const String iconsArrowDownThick = 'assets/icons/arrow_down_thick.svg';
   static const String iconsArrowLeft = 'assets/icons/arrow_left.svg';
   static const String iconsArrowLeftThick = 'assets/icons/arrow_left_thick.svg';
   static const String iconsArrowRight = 'assets/icons/arrow_right.svg';
-  static const String iconsArrowRightThick =
-      'assets/icons/arrow_right_thick.svg';
+  static const String iconsArrowRightThick = 'assets/icons/arrow_right_thick.svg';
   static const String iconsArrowUp = 'assets/icons/arrow_up.svg';
   static const String iconsArrowUpThick = 'assets/icons/arrow_up_thick.svg';
   static const String iconsAvatar1 = 'assets/icons/avatar_1.svg';
@@ -47,12 +47,10 @@ class Assets {
   static const String iconsClose = 'assets/icons/close.svg';
   static const String iconsCloseThick = 'assets/icons/close_thick.svg';
   static const String iconsCorrect = 'assets/icons/correct.svg';
-  static const String iconsCorrectIndicator =
-      'assets/icons/correct_indicator.svg';
+  static const String iconsCorrectIndicator = 'assets/icons/correct_indicator.svg';
   static const String iconsDataStructure = 'assets/icons/data_structure.svg';
   static const String iconsErrorIndicator = 'assets/icons/error_indicator.svg';
-  static const String iconsExpansionArrowIndicator =
-      'assets/icons/expansion_arrow_indicator.svg';
+  static const String iconsExpansionArrowIndicator = 'assets/icons/expansion_arrow_indicator.svg';
   static const String iconsGoogleLogo = 'assets/icons/google_logo.svg';
   static const String iconsHome = 'assets/icons/home.svg';
   static const String iconsIncorrect = 'assets/icons/incorrect.svg';
@@ -63,31 +61,22 @@ class Assets {
   static const String iconsPencil = 'assets/icons/pencil.svg';
   static const String iconsPlus = 'assets/icons/plus.svg';
   static const String iconsPlusThick = 'assets/icons/plus_thick.svg';
-  static const String iconsRoundBlueCircle =
-      'assets/icons/round_blue_circle.svg';
-  static const String iconsRoundedBlueExclamation =
-      'assets/icons/rounded_blue_exclamation.svg';
+  static const String iconsRoundBlueCircle = 'assets/icons/round_blue_circle.svg';
+  static const String iconsRoundedBlueExclamation = 'assets/icons/rounded_blue_exclamation.svg';
   static const String iconsRoundedCamera = 'assets/icons/rounded_camera.svg';
   static const String iconsRoundedCheck = 'assets/icons/rounded_check.svg';
-  static const String iconsRoundedCheckSmallBlue =
-      'assets/icons/rounded_check_small_blue.svg';
-  static const String iconsRoundedCheckThick =
-      'assets/icons/rounded_check_thick.svg';
+  static const String iconsRoundedCheckSmallBlue = 'assets/icons/rounded_check_small_blue.svg';
+  static const String iconsRoundedCheckThick = 'assets/icons/rounded_check_thick.svg';
   static const String iconsRoundedClose = 'assets/icons/rounded_close.svg';
-  static const String iconsRoundedCloseBlue =
-      'assets/icons/rounded_close_blue.svg';
-  static const String iconsRoundedCloseSmallRed =
-      'assets/icons/rounded_close_small_red.svg';
-  static const String iconsRoundedCloseThick =
-      'assets/icons/rounded_close_thick.svg';
-  static const String iconsRoundedExclamation =
-      'assets/icons/rounded_exclamation.svg';
+  static const String iconsRoundedCloseBlue = 'assets/icons/rounded_close_blue.svg';
+  static const String iconsRoundedCloseSmallRed = 'assets/icons/rounded_close_small_red.svg';
+  static const String iconsRoundedCloseThick = 'assets/icons/rounded_close_thick.svg';
+  static const String iconsRoundedExclamation = 'assets/icons/rounded_exclamation.svg';
   static const String iconsRoundedMore = 'assets/icons/rounded_more.svg';
   static const String iconsRoundedOs = 'assets/icons/rounded_os.svg';
   static const String iconsRoundedPlusBig = 'assets/icons/rounded_plus_big.svg';
   static const String iconsRoundedTop = 'assets/icons/rounded_top.svg';
-  static const String iconsRoundedWarnningSmallRed =
-      'assets/icons/rounded_warnning_small_red.svg';
+  static const String iconsRoundedWarnningSmallRed = 'assets/icons/rounded_warnning_small_red.svg';
   static const String iconsSearch = 'assets/icons/search.svg';
   static const String iconsSearchThick = 'assets/icons/search_thick.svg';
   static const String iconsSend = 'assets/icons/send.svg';
@@ -104,35 +93,25 @@ class Assets {
   static const String imagesSparkle = 'assets/images/sparkle.svg';
   static const String imagesSplashImage = 'assets/images/splash_image.png';
   static const String imagesTopicAndroid = 'assets/images/topic_android.png';
-  static const String imagesTopicDataStructure =
-      'assets/images/topic_data_structure.png';
+  static const String imagesTopicDataStructure = 'assets/images/topic_data_structure.png';
   static const String imagesTopicDatabase = 'assets/images/topic_database.png';
   static const String imagesTopicFlutter = 'assets/images/topic_flutter.png';
   static const String imagesTopicIos = 'assets/images/topic_ios.png';
   static const String imagesTopicJava = 'assets/images/topic_java.png';
-  static const String imagesTopicJavascript =
-      'assets/images/topic_javascript.png';
+  static const String imagesTopicJavascript = 'assets/images/topic_javascript.png';
   static const String imagesTopicNestJs = 'assets/images/topic_nest_js.png';
   static const String imagesTopicNetwork = 'assets/images/topic_network.png';
-  static const String imagesTopicOperatingSystem =
-      'assets/images/topic_operating_system.png';
+  static const String imagesTopicOperatingSystem = 'assets/images/topic_operating_system.png';
   static const String imagesTopicReact = 'assets/images/topic_react.png';
   static const String imagesTopicSpring = 'assets/images/topic_spring.png';
   static const String imagesTopicSwift = 'assets/images/topic_swift.png';
-  static const String imagesTopicWebFrontend =
-      'assets/images/topic_webFrontend.png';
-  static const String imagesWelcomeTechtalk =
-      'assets/images/welcome_techtalk.svg';
+  static const String imagesTopicWebFrontend = 'assets/images/topic_webFrontend.png';
+  static const String imagesWelcomeTechtalk = 'assets/images/welcome_techtalk.svg';
   static const String jsonSkills = 'assets/json/skills.json';
-  static const String jsonTopicCategoriesData =
-      'assets/json/topic_categories_data.json';
-  static const String pretendardPretendardBold =
-      'assets/fonts/pretendard/Pretendard-Bold.otf';
-  static const String pretendardPretendardMedium =
-      'assets/fonts/pretendard/Pretendard-Medium.otf';
-  static const String pretendardPretendardRegular =
-      'assets/fonts/pretendard/Pretendard-Regular.otf';
-  static const String pretendardPretendardSemiBold =
-      'assets/fonts/pretendard/Pretendard-SemiBold.otf';
+  static const String jsonTopicCategoriesData = 'assets/json/topic_categories_data.json';
+  static const String pretendardPretendardBold = 'assets/fonts/pretendard/Pretendard-Bold.otf';
+  static const String pretendardPretendardMedium = 'assets/fonts/pretendard/Pretendard-Medium.otf';
+  static const String pretendardPretendardRegular = 'assets/fonts/pretendard/Pretendard-Regular.otf';
+  static const String pretendardPretendardSemiBold = 'assets/fonts/pretendard/Pretendard-SemiBold.otf';
   static const String techTalk = '.env';
 }
