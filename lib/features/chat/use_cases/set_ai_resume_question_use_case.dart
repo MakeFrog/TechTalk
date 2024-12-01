8612e031985f9f559669417a8f4399eb3013d87c
yundal <soneymarr@gmail.com>
2024-12-02 01:42:27 +0900

    feature : 4o 모델을 적용한 프롬프트 제작

--- lib/features/chat/use_cases/set_ai_resume_question_use_case.dart
+++ lib/features/chat/use_cases/set_ai_resume_question_use_case.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:techtalk/core/index.dart';
 import 'package:techtalk/features/chat/repositories/entities/resume_qna_entity.dart';
 
@@ -12,31 +13,60 @@ class SetAiResumeQuestionUseCase
   @override
   Future<Result<List<ResumeQnaEntity>>> call(GetResumeParam request) async {
     try {
-      
-      // GPT 응답을 대기
+      // GPT 응답 대기
       final response = await OpenAI.instance.onChatCompletion(
         request: ChatCompleteText(
           messages: createResumeChatMessage(request),
-          functionCall: FunctionCall.auto,
-          maxToken: 300,
+          maxToken: 800,
           temperature: 0.5,
           user: FirebaseAuth.instance.currentUser!.uid,
-          model: Gpt4ChatModel(),
+          model: Gpt4OChatModel(),
         ),
       );
+
+      // 응답의 토큰 정보 출력
+      if (response != null) {
+        final totalTokens = response.usage?.totalTokens ?? 0;
+        final promptTokens = response.usage?.promptTokens ?? 0;
+        final completionTokens = response.usage?.completionTokens ?? 0;
+
+        debugPrint('===== 토큰 사용량 =====');
+        debugPrint('총 토큰 수: $totalTokens');
+        debugPrint('프롬프트 토큰 수: $promptTokens');
+        debugPrint('완성 토큰 수: $completionTokens');
+      } else {
+        debugPrint('GPT 응답이 없습니다.');
+      }
 
       // JSON 파싱 및 결과 매핑
       final parsedResponse = jsonDecode(response!.choices[0].message!.content);
       final List<ResumeQnaEntity> qnaList = (parsedResponse as List)
-          .map((json) => ResumeQnaEntity(
-                id: json['id'] ?? '',
-                question: json['question'],
-                questionType: json['type'] == 'hardSkill'
-                    ? ResumeQuestionType.hardSkill
-                    : ResumeQuestionType.softSkill,
-                evaluationPoint: json['evaluationPoint'] ?? '',
-              ))
+          .map(
+            (json) => ResumeQnaEntity(
+              id: json['id'] ?? '',
+              question: json['question'],
+              questionType: json['type'] == 'hardSkill'
+                  ? ResumeQuestionType.hardSkill
+                  : ResumeQuestionType.softSkill,
+              evaluationPoint: json['evaluationPoint'] ?? '',
+            ),
+          )
           .toList();
+
+      debugPrint(
+        const JsonEncoder.withIndent('').convert(
+          qnaList.map((qna) {
+            return {
+              "id": qna.id,
+              "question": qna.question,
+              "type": qna.questionType == ResumeQuestionType.hardSkill
+                  ? "hardSkill"
+                  : "softSkill",
+              "evaluationPoint": qna.evaluationPoint,
+            };
+          }).toList(),
+        ),
+      );
 
       // 성공적으로 결과 반환
       return Result.success(qnaList);
@@ -49,29 +79,37 @@ class SetAiResumeQuestionUseCase
   /// GPT 프롬프트 메시지 생성
   List<Map<String, dynamic>> createResumeChatMessage(GetResumeParam request) {
     final String promptContent = '''
-  당신은 이력서 및 포트폴리오 분석을 도와주는 전문가입니다. 아래는 지원자의 이력서 내용입니다:
+  당신은 개발자를 채용하는 회사의 채용 전문가입니다. 아래는 회사에 지원한 지원자의 이력서와 포트폴리오 내용입니다.
 
-  "${request.resumeOrPortfolioContent}"
+  이력서와 포트폴리오의 내용은 지원자의 주요 기술 스택, 프로젝트 기여도, 그리고 문제 해결 과정에 초점을 맞추고 있습니다.
+  질문 작성 시 지원자가 수행한 프로젝트, 해결한 도전 과제, 적용한 기술과 관련된 맥락을 반영하세요.
 
-  이 내용을 바탕으로 하드스킬(기술적 역량)과 소프트스킬(비기술적 역량)을 구분하여 질문을 작성하세요. 질문은 아래 형식을 따르세요:
+  이력서 내용
+  "${request.resumeContent}"
 
-  [
-    {
-      "question": "Flutter와 네이티브 코드를 통합하여 로그인 기능을 구현한 사례를 설명해주세요.",
-      "type": "hardSkill",
-      "evaluationPoint": "Flutter와 네이티브 통합 기술, Firebase Auth 활용 능력"
-    },
-    {
-      "question": "팀 협업 과정에서 어려움을 겪었을 때 이를 어떻게 극복했는지 설명해주세요.",
-      "type": "softSkill",
-      "evaluationPoint": "협업 능력, 갈등 해결 및 문제 극복 역량"
-    }
-  ]
+  포트폴리오 내용
+  "${request.portfolioContent}"
 
   작성 규칙:
-  1. 질문 유형은 "hardSkill" 또는 "softSkill" 중 하나여야 합니다.
-  2. 각 질문마다 "question", "type", "evaluationPoint" 필드를 포함해야 합니다.
-  3. 지원자의 경험과 기술에 맞는 현실적이고 구체적인 질문을 작성해주세요.
+  - 총 질문의 개수는 4~8개이며, 이력서와 포트폴리오의 내용에 따라 달라집니다.
+  - 질문은 기술적 역량, 비즈니스 성과, 협업 방식, 창의적 문제 해결 능력 등을 평가할 수 있도록 다양하게 작성하세요.
+  - 하드스킬 질문은 기술의 사용 사례, 문제 해결 과정, 성과에 대해 묻도록 작성하세요.
+  - 소프트스킬 질문은 팀 협업, 프로젝트 관리, 의사소통 능력 등을 평가할 수 있는 내용으로 작성하세요.
+  - 평가 기준(evaluationPoint)은 지원자의 역량을 평가할 구체적인 기술적/소프트스킬 기준을 포함하며, 50자 이내로 작성하세요.
+
+  예시:
+  [
+    {
+      "question": "클린 아키텍처를 적용하여 코드 리뷰 시간을 단축한 방법을 설명해주세요.",
+      "type": "hardSkill",
+      "evaluationPoint": "클린 아키텍처 이해도, 코드 품질 개선 능력"
+    },
+    {
+      "question": "팀 프로젝트에서 UI/UX 개선을 위해 디자이너와 협력한 경험을 공유해주세요.",
+      "type": "softSkill",
+      "evaluationPoint": "팀 협업 능력, 창의적 문제 해결 능력"
+    }
+  ]
   ''';
 
     return [
@@ -82,6 +120,4 @@ class SetAiResumeQuestionUseCase
 }
 
 /// 파라미터
-typedef GetResumeParam = ({
-  String resumeOrPortfolioContent,
-});
+typedef GetResumeParam = ({String resumeContent, String portfolioContent});
--- lib/presentation/pages/home/widgets/test_resume_interview_card.dart
+++ lib/presentation/pages/home/widgets/test_resume_interview_card.dart
@@ -12,22 +12,8 @@ class TestResumeInterviewCard extends ConsumerWidget with HomeState, HomeEvent {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // 임시 프롬프팅 파라미터
-    const String resumeOrPortfolioContent = '''
-저는 5년 이상의 Flutter 개발 경험이 있으며, riverpod와 provider를 활용한 상태 관리에 능숙합니다. GoRouter를 사용하여 복잡한 내비게이션 구조를 구현한 경험이 있고, 3개의 Flutter 앱을 성공적으로 출시하여 앱 스토어에서 긍정적인 평가를 받았습니다.
-
-또한, RESTful API와 GraphQL을 활용한 백엔드 연동 경험이 있으며, Firebase를 이용한 인증 및 데이터베이스 관리에 익숙합니다. 팀 협업을 위한 Git 및 CI/CD 파이프라인 구축 경험도 가지고 있습니다.
-
-UI/UX 디자인에도 관심이 많아 Material Design과 Cupertino 디자인 가이드를 준수하여 사용자 친화적인 인터페이스를 구현할 수 있습니다. 문제 해결 능력이 뛰어나며, 새로운 기술을 학습하고 적용하는 데 적극적입니다.
-''';
-
     return BounceTapper(
-      onTap: () {
-        testSetAiResumeQuestionUseCase(
-          isFile: false,
-          resumeOrPortfolioContent: resumeOrPortfolioContent,
-        );
-      },
+      onTap: testSetAiResumeQuestionUseCase,
       child: Container(
         padding: const EdgeInsets.fromLTRB(24, 12, 0, 12),
         decoration: BoxDecoration(
