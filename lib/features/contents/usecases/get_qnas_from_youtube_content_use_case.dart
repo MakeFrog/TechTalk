646b8509e7ec3ae3e5266ce095ee1abfe7e7532a
XimYa <hj_lee_com@naver.com>
2024-12-31 21:18:07 +0900

    feature : 유튜브 콘텐츠기반 면접 질문 생성 프롬프트 useCase 모듈 개발

---
+++ lib/features/contents/usecases/get_qnas_from_youtube_content_use_case.dart
@@ -0,0 +1,117 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dart_openai/dart_openai.dart';
+import 'package:techtalk/app/localization/app_locale.dart';
+import 'package:techtalk/core/index.dart';
+import 'package:techtalk/features/chat/repositories/entities/youtube_qna_entity.dart';
+import 'package:techtalk/features/contents/repositories/entities/youtube_ai_qna_response.dart';
+import 'package:techtalk/features/contents/repositories/entities/youtube_video_entity.dart';
+import 'package:techtalk/features/contents/usecases/exception/youtube_ai_analyze_exception.dart';
+import 'package:techtalk/features/tech_set/tech_set.dart';
+
+class GetQnasFromYoutubeContentUseCase
+    extends BaseUseCase<YoutubeVideoAndCaptionEntity, YoutubeAiQnaResponse> {
+  @override
+  Future<YoutubeAiQnaResponse> call(
+      YoutubeVideoAndCaptionEntity request) async {
+    // the system message that will be sent to the request.
+    final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+          '''
+프로그래밍 관련 유튜브 영상을 기반으로 기술 면접 질문을 생성하세요.
+
+영상의 제목과 내용을 바탕으로 적합한 면접 질문과 모범답안을 작성하세요.
+
+### 요구사항:
+1. **응답 유형 (`type`) 지정**:
+   - **`notTech`**: 영상 내용이 프로그래밍과 관련이 없는 경우.
+   - **`lackOfContent`**: 영상 내용이 충분히 기술적이지 않거나, 면접 질문을 만들기에 적합하지 않은 경우. ex) 개발자 인터뷰, 브이로그.
+   - **`isValid`**: 영상 내용에서 기술 면접 질문을 생성할 수 있는 경우.
+
+2. **질문(`qnas`) 작성**:
+   - 영상에서 다루고 있는 프로그래밍 개념을 기반으로 면접 질문을 최소 4개에서 최대 12개까지 작성하세요.
+   - 질문은 영상에서 다루는 프로그래밍 개념을 물어보는 내용으로 구성하세요.
+   - 모범답안은 영상의 내용을 기반으로 명확하고 구체적으로 작성하세요.
+   - 적절한 질문이 없을 경우 빈 배열을 반환하세요.
+
+---
+
+### 입력 데이터 형식:
+- **제목**: `${request.title}`
+- **내용**: `${request.script}`
+
+### 응답 언어:
+- 언어 코드에 해당되는 언어로 응답하세요.
+- 언어 코드: `${AppLocale.currentLocale.languageCode}`
+
+### 응답 어체:
+- 경어체를 사용합니다.(존댓말)
+
+### 응답 형식:
+아래 JSON 구조를 따르세요.
+  
+```json
+{
+  "type": "notTech | lackOfContent | isValid", // 필수
+  "qnas": [ // 필수
+    { 
+      "question": "면접 질문", // 필수
+      "answer": "모범 답안" // 필수
+    }
+  ]
+} 
+          ''',
+        ),
+      ],
+      role: OpenAIChatMessageRole.system,
+    );
+
+    final startTime = DateTime.now();
+
+    try {
+      OpenAIChatCompletionModel completion = await OpenAI.instance.chat.create(
+        model: "gpt-4o",
+        messages: [
+          systemMessage,
+        ],
+        responseFormat: {"type": "json_object"},
+        temperature: 0.8,
+      );
+
+      log('Qna 토큰사용량 : ${completion.usage}'); // 응답 결과 출력
+      log('Qna 시간 : ${DateTime.now().difference(startTime).inSeconds}'); // 응답 결과 출력
+      log('Qna 결과랑이 : ${completion.choices.first.message.content?.first.text}'); // 응답 결과 출력
+      log('Qna 결과 : ${completion.choices.first.message.content}'); // 응답 결과 출력
+
+      final response = completion.choices.first.message.content?.first.text;
+
+      if (response == null) {
+        throw const YtUnexceptedGptException();
+      }
+
+      final targetJson = jsonDecode(response);
+
+      final targetEntity = YoutubeAiQnaResponse.fromJson(targetJson);
+      print('이찌방 qna 성공 : ${targetEntity}');
+      return targetEntity;
+    } on RequestFailedException catch (e) {
+      log('GetSummaryFromYoutubeContentUseCase : $e');
+      throw const YtUnexceptedGptException();
+    } catch (e) {
+      if (e is TimeoutException) {
+        log('GetSummaryFromYoutubeContentUseCase / TimeoutException / $e');
+        throw const YtTimeoutException();
+      } else if (e is FormatException) {
+        log('GetSummaryFromYoutubeContentUseCase / FormatException :$e');
+        throw const YtJsonFormatException();
+      }
+
+      log('GetSummaryFromYoutubeContentUseCase : 그 외 오류 :$e');
+
+      throw const YtUnknownException();
+    }
+  }
+}
