72df5642fb85556bbdbd591c8a19c7f37282f869
XimYa <hj_lee_com@naver.com>
2025-01-02 20:37:09 +0900

    feature : 페이지네이션 호출 예외 경우 ui 예외처리 작업

--- lib/presentation/pages/youtube/main/widgets/content_list_view.p.dart
+++ lib/presentation/pages/youtube/main/widgets/content_list_view.p.dart
@@ -39,51 +39,66 @@ class _ContentListView extends HookConsumerWidget
                     ),
                   );
                 },
-                firstPageProgressIndicatorBuilder: (context) => Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ...List.generate(
-                      6,
-                      (_) => Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: YoutubeContentItemView.createSkeleton()),
-                    ),
-                  ],
-                ),
-                newPageProgressIndicatorBuilder: (context) =>
-                    Center(child: CircularProgressIndicator()),
-                firstPageErrorIndicatorBuilder: (context) => Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('데이터 로딩 중 오류가 발생했습니다.'),
-                      SizedBox(height: 8),
-                      ElevatedButton(
-                        onPressed: () => targetController.refresh(),
-                        child: Text('다시 시도'),
-                      ),
-                    ],
-                  ),
-                ),
-                newPageErrorIndicatorBuilder: (context) => Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('추가 데이터 로딩 중 오류가 발생했습니다.'),
-                      SizedBox(height: 8),
-                      ElevatedButton(
-                        onPressed: () =>
-                            targetController.retryLastFailedRequest(),
-                        child: Text('다시 시도'),
-                      ),
-                    ],
-                  ),
-                ),
-                noItemsFoundIndicatorBuilder: (context) =>
-                    Center(child: Text('데이터가 없습니다.')),
+                firstPageProgressIndicatorBuilder: (_) => _buildLoadView(),
+                newPageProgressIndicatorBuilder: (_) =>
+                    const Center(child: CircularProgressIndicator()),
+                firstPageErrorIndicatorBuilder: (_) =>
+                    _buildErrorOccuredView(targetController),
+                newPageErrorIndicatorBuilder: (_) =>
+                    _buildErrorOccuredView(targetController),
+                noItemsFoundIndicatorBuilder: _buildNoItemFoundView,
               ),
             );
           }),
+    );
+  }
+
+  ///
+  /// 호출 중 오류 발생
+  ///
+  Widget _buildErrorOccuredView(
+      PagingController<DocumentSnapshot<YoutubeMainModel>?,
+              YoutubeContentOverviewEntity>
+          controller) {
+    return YoutubePaginationIndicatorView(
+      title: '영상을 불러오지 못했어요',
+      description: '일시적이 오류일 수 있으니 다시 시도해보세요',
+      btnText: '다시 시도',
+      onBtnTapped: () {
+        controller.refresh();
+      },
+    );
+  }
+
+  ///
+  /// 검색된 항목 없음
+  ///
+  Widget _buildNoItemFoundView(BuildContext context) {
+    return YoutubePaginationIndicatorView(
+      title: '결과가 없습니다',
+      description: '영상을 업로드해 보세요\n영상 요약 및 질문을 생성해 드립니다',
+      btnText: '영상 업로드하기',
+      onBtnTapped: () {
+        onVideoUploadBtnTapped(context);
+      },
+    );
+  }
+
+  ///
+  /// skeleton 로딩
+  ///
+  Widget _buildLoadView() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ...List.generate(
+          6,
+          (_) => Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: YoutubeContentItemView.createSkeleton(),
+          ),
+        ),
+      ],
     );
   }
 }
