0611d29a8f96e0c9955a4a7e7dd869a330b2388c
XimYa <hj_lee_com@naver.com>
2025-01-19 22:11:56 +0900

    refactor : UI 구조화

--- lib/presentation/pages/study/topic_selection/study_topic_selection_page.dart
+++ lib/presentation/pages/study/topic_selection/study_topic_selection_page.dart
@@ -15,6 +15,12 @@ import 'package:techtalk/presentation/widgets/base/base_page.dart';
 import 'package:techtalk/presentation/widgets/common/app_bar/foldable_app_bar.dart';
 import 'package:techtalk/presentation/widgets/section/study_topic_card.dart';
 
+part 'widgets/scaffold.p.dart';
+
+part 'widgets/wrong_answer_card.p.dart';
+
+part 'widgets/study_topic_grid_view.p.dart';
+
 class StudyTopicSelectionPage extends BasePage
     with StudyTopicSelectionState, StudyTopicSelectionEvent {
   const StudyTopicSelectionPage({super.key});
@@ -23,98 +29,9 @@ class StudyTopicSelectionPage extends BasePage
   Widget buildPage(BuildContext context, WidgetRef ref) {
     useAutomaticKeepAlive();
 
-    return SingleChildScrollView(
-      controller: scrollController(ref),
-      physics: const ScrollPhysics(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            const Gap(8),
-            BounceTapper(
-              onTap: () {
-                onWrongAnswerCardTapped(ref);
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: AppColor.of.red1,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                alignment: Alignment.topLeft,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      flex: 187,
-                      child: Container(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  '오답노트',
-                                  style: AppTextStyle.headline2.copyWith(
-                                    color: AppColor.of.red2,
-                                  ),
-                                ),
-                                const Gap(2),
-                                SvgPicture.asset(
-                                  Assets.iconsRightAlignedRightArrow,
-                                )
-                              ],
-                            ),
-                            const Gap(4),
-                            Text(
-                              '오답 복습을 통해\n실력을 증진하세요!',
-                              style: AppTextStyle.body3.copyWith(
-                                color: AppColor.of.gray5,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 156,
-                      child: SvgPicture.asset(
-                        Assets.iconsMistakeNoteIllust,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 11,
-                mainAxisSpacing: 12,
-              ),
-              itemCount: topics(ref).length,
-              itemBuilder: (context, index) {
-                final topic = topics(ref)[index];
-
-                return StudyTopicCard(
-                  topic: topic,
-                  onTap: () => onTapCard(
-                    ref,
-                    topic: topic,
-                  ),
-                  scrollController: scrollController(ref),
-                );
-              },
-            ),
-            const Gap(120),
-          ],
-        ),
-      ),
+    return const _Scaffold(
+      wrongAnswerNoteCard: _WrongAnswerNoteCard(),
+      studyTopicGridView: _StudyTopicGridView(),
     );
   }
 
