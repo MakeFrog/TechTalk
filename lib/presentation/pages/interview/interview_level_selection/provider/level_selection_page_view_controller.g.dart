5af0164d4445d652b00dfebe6cc1db8087e46d75
XimYa <hj_lee_com@naver.com>
2025-03-02 18:07:33 +0900

    feature : 테크셋, 면접 질문 난이도 선택 페이지 UI 레이아웃 작업

---
+++ lib/presentation/pages/interview/interview_level_selection/provider/level_selection_page_view_controller.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'level_selection_page_view_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$levelSelectionPageViewControllerHash() =>
+    r'fec76f16c99919771a9b02211292e388ef9c1d75';
+
+/// See also [LevelSelectionPageViewController].
+@ProviderFor(LevelSelectionPageViewController)
+final levelSelectionPageViewControllerProvider = AutoDisposeNotifierProvider<
+    LevelSelectionPageViewController, PageController>.internal(
+  LevelSelectionPageViewController.new,
+  name: r'levelSelectionPageViewControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$levelSelectionPageViewControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LevelSelectionPageViewController
+    = AutoDisposeNotifier<PageController>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
