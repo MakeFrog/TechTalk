d169dd00b2171f639bc5566d9b53ac134bb87ff0
XimYa <hj_lee_com@naver.com>
2025-03-30 22:37:47 +0900

    refactor : 불필요 코드 제거

--- lib/presentation/pages/interview/select_commotion_interview_type/selected_common_interview_type_state.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-mixin SelectedCommonInterviewTypeState {
-  // TODO: Add state variables and methods
-}
